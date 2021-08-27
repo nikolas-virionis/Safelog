57bdc79f00c30ff15526c84d739d1cf370adc332
nikolas-virionis <nikolas.virionis@bandtec.com.br>
2021-08-26 22:50:32 -0300

    debug idade testRoute

--- site-estatico/routes/test.js
+++ site-estatico/routes/test.js
@@ -1,40 +1,39 @@
 // dependencias
-let express = require('express');
+let express = require("express");
 let router = express.Router();
 // let sequielize = require('../models').sequelize;
 
-let env = process.env.NODE_ENV || 'development';
+let env = process.env.NODE_ENV || "development";
 
 // exemplo de rota
-router.get('/testRoute', function(req, res, next) {
+router.get("/testRoute", function (req, res, next) {
+  // exemplo de resposta em json
+  let obj = {
+    nome: "Bikolas",
+    sobrenome: "Narcha",
+    idade: 420,
+  };
 
-    // exemplo de resposta em json
-    let obj = {
-        nome: "Bikolas",
-        sobrenome: "Narcha",
-        idade: 320
-    }
-    
-    res.json(obj)
+  res.json(obj);
 
-    // exemplo de consulta no banco
-    // let sql = "";
+  // exemplo de consulta no banco
+  // let sql = "";
 
-	// if (env == "dev") {
-	// 	sql = "SELECT * FROM table;";
-	// } else {
-    //     sql = "";
-    // }
+  // if (env == "dev") {
+  // 	sql = "SELECT * FROM table;";
+  // } else {
+  //     sql = "";
+  // }
 
-	// sequelize.query(instrucaoSql, {
-	// 	type: sequelize.QueryTypes.SELECT
-	// })
-	// .then(resultado => {
-	// 	res.json(resultado);
-	// }).catch(erro => {
-	// 	console.error(erro);
-	// 	res.status(500).send(erro.message);
-	// })
-})
+  // sequelize.query(instrucaoSql, {
+  // 	type: sequelize.QueryTypes.SELECT
+  // })
+  // .then(resultado => {
+  // 	res.json(resultado);
+  // }).catch(erro => {
+  // 	console.error(erro);
+  // 	res.status(500).send(erro.message);
+  // })
+});
 
 module.exports = router;

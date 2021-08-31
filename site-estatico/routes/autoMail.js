f8785ecd6ab434227ea98a44ad4ed2cdf14692af
nikolas-virionis <nikolas.virionis@bandtec.com.br>
2021-08-31 19:23:43 -0300

    merge branch email-node + rename pasta email-sender

--- site-estatico/routes/autoMail.js
+++ site-estatico/routes/autoMail.js
@@ -1,21 +1,21 @@
 // dependencias
 let express = require("express");
 let router = express.Router();
-let send = require("../util/msg-streak-shooter/script").mandarEmail;
+let send = require("../util/email-sender/script").mandarEmail;
 // let sequielize = require('../models').sequelize;
 
 let env = process.env.NODE_ENV || "development";
 
 // send email
 router.post("/sendMail", (req, res, next) => {
-    let body = req.body
-    // console.log(body.nome)
-    send(body.tipo, body.nome, body.remetente, body.destinatario, body.senha)
-    
-    res.json({
-      "status": "ok"
-    })
-})
+  let body = req.body;
+  // console.log(body.nome)
+  send(body.tipo, body.nome, body.remetente, body.destinatario, body.senha);
+
+  res.json({
+    status: "ok",
+  });
+});
 
 // exemplo de rota
 router.get("/testRoute", function (req, res, next) {

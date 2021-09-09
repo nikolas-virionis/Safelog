92107fb923f230300ea06fa46cea4ff0a47e87de
nikolas-virionis <nikolas.virionis@bandtec.com.br>
2021-09-08 21:20:42 -0300

    favicon global (js) + debug caminho img logo

--- site-estatico/public/assets/js/script.js
+++ site-estatico/public/assets/js/script.js
@@ -1,2 +1,6 @@
-var currentYear= new Date().getFullYear(); 
+var currentYear = new Date().getFullYear();
 document.getElementById("footer-year").innerHTML = currentYear;
+const link = document.createElement("link");
+link.rel = "shortcut icon";
+link.href = "assets/img/logo/logo-icon-branco.png";
+document.getElementsByTagName("head")[0].appendChild(link);

6f4e319f6b435a7c8237b6edbe7163e0666dec6f
LucasTeixeira793 <lucas.teixeira@bandtec.com.br>
2021-09-11 20:08:16 -0300

    Página de cadastro de pessoas

--- site-estatico/public/assets/js/script.js
+++ site-estatico/public/assets/js/script.js
@@ -1,6 +1,13 @@
-var currentYear = new Date().getFullYear();
-document.getElementById("footer-year").innerHTML = currentYear;
+
 const link = document.createElement("link");
 link.rel = "shortcut icon";
 link.href = "assets/img/logo/logo-icon-branco.png";
 document.getElementsByTagName("head")[0].appendChild(link);
+
+function fecharModal(){
+    document.getElementById("modal-verify-token").style.display = "none";
+}
+
+function cancelarModal(){
+    window.history.back();
+}

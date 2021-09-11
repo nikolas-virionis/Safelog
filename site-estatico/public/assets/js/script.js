1de225258f2f5bddb2ab313e0d0a74095acedb85
nikolas-virionis <nikolas.virionis@bandtec.com.br>
2021-09-11 16:00:05 -0300

    fix para favicon nas paginas sem footer

--- site-estatico/public/assets/js/script.js
+++ site-estatico/public/assets/js/script.js
@@ -1,6 +1,10 @@
-var currentYear = new Date().getFullYear();
-document.getElementById("footer-year").innerHTML = currentYear;
-const link = document.createElement("link");
-link.rel = "shortcut icon";
-link.href = "assets/img/logo/logo-icon-branco.png";
-document.getElementsByTagName("head")[0].appendChild(link);
+try {
+  var currentYear = new Date().getFullYear();
+  document.getElementById("footer-year").innerHTML = currentYear;
+} catch (e) {
+} finally {
+  const link = document.createElement("link");
+  link.rel = "shortcut icon";
+  link.href = "assets/img/logo/logo-icon-branco.png";
+  document.getElementsByTagName("head")[0].appendChild(link);
+}

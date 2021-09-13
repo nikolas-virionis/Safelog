cd6aba7688f8cbe447f527a7b87e99acb2d765cd
nikolas-virionis <nikolas.virionis@bandtec.com.br>
2021-09-13 16:40:02 -0300

    criação footer.js e aplicação em todas as paginas que usam o footer

--- site-estatico/public/assets/js/script.js
+++ site-estatico/public/assets/js/script.js
@@ -6,8 +6,3 @@ document.getElementsByTagName("head")[0].appendChild(link);
 const fecharModal = () =>
   (document.getElementById("modal-verify-token").style.display = "none");
 const cancelarModal = () => window.history.back();
-
-try {
-  var currentYear = new Date().getFullYear();
-  document.getElementById("footer-year").innerHTML = currentYear;
-} catch (e) {}

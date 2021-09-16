9d36eae4df95a9cc7d0e1c9b6f0d45a7cf52d971
nikolas-virionis <nikolas.virionis@bandtec.com.br>
2021-09-16 16:59:55 -0300

    correção crases comandos de criação do user

--- banco-de-dados/create_sql_user.sql
+++ banco-de-dados/create_sql_user.sql
@@ -1,5 +1,5 @@
 CREATE USER 'safelog_dev' @'localhost' IDENTIFIED BY 'Safe_Log371$';
 
-GRANT ALL PRIVILEGES ON 'safelog'.* TO 'safelog_dev' @'localhost';
+GRANT ALL PRIVILEGES ON `safelog`.* TO `safelog_dev` @`localhost`;
 
 FLUSH PRIVILEGES;

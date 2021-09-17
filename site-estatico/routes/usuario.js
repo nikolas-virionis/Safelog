c738f9328a3eafa3f49384e764f2e9098178e49c
nikolas-virionis <nikolas.virionis@bandtec.com.br>
2021-09-17 20:43:53 -0300

    logica backend cadastro final => update usuario + cadastro formas de contato escolhidas

--- site-estatico/routes/usuario.js
+++ site-estatico/routes/usuario.js
@@ -1,7 +1,11 @@
 // dependencias
 let express = require("express");
 let router = express.Router();
-let {
+const {
+    updateDadosUsuario,
+    criarFormasContato,
+} = require("../util/cadastro-final/updateCadastro");
+const {
     checarEmStaff,
     checarEmUsuario,
     gerarId,
@@ -14,8 +18,10 @@ router.get("/cadastro", (req, res, next) =>
 );
 
 router.post("/convite", async (req, res, next) => {
+    // body da requisição post => dados principais da rota
     const { email, cargo, fk_empresa, fk_supervisor, complementos } = req.body;
     if (!email) return res.status(403).send("body não fornecido na requisição");
+    //checar se email existe em staff ou usuario
     let emStaff;
     let emUsuario;
     await checarEmStaff(email).then((bool) => (emStaff = bool));
@@ -23,13 +29,42 @@ router.post("/convite", async (req, res, next) => {
         return res.status(403).send("Usuário já registrado como staff");
     await checarEmUsuario(email).then((bool) => (emUsuario = bool));
     if (emUsuario) return res.status(403).send("Usuário já registrado");
+    // geração do id unico
     let hash;
-    await gerarId().then((id) => (hash = id)); //hash = id
+    await gerarId().then((id) => (hash = id));
+    // insert parcial de dados
     await insertParcial(hash, email, cargo, fk_empresa, fk_supervisor);
+    // email de cadastr enviado para
     await send("cadastro", undefined, complementos[0], email, complementos[1], [
         hash,
     ]);
 
+    return res.json({
+        status: "ok",
+    });
+});
+
+router.post("/cadastro-final", async (req, res, next) => {
+    const { id, nome, email, senha, contatos } = req.body;
+
+    await updateDadosUsuario(id, nome, email, senha)
+        .then((response) => console.log(response))
+        .catch((err) => {
+            console.log(err);
+            res.json({
+                status: "error",
+                msg: err,
+            });
+        });
+    await criarFormasContato(id, contatos)
+        .then((response) => console.log(response))
+        .catch((err) => {
+            console.log(err);
+            res.json({
+                status: "error",
+                msg: err,
+            });
+        });
     return res.json({
         status: "ok",
     });
---
+++ site-estatico/util/cadastro-final/updateCadastro.js
@@ -0,0 +1,26 @@
+let sequelize = require("../../models").sequelize;
+
+const updateDadosUsuario = async (id, nome, email, senha) => {
+    let sql = `UPDATE usuario SET nome = '${nome}', email = '${email}', senha = MD5('${senha}') WHERE id_usuario = '${id}';`;
+    await sequelize.query(sql, { type: sequelize.QueryTypes.UPDATE });
+    return { status: "ok" };
+};
+
+const criarFormasContato = async (id, contatos) => {
+    let forma = 1;
+    Object.entries(contatos).forEach(async ([key, value]) => {
+        if (key == "whatsapp") {
+            let sql = `INSERT INTO contato VALUES ('${id}', ${forma++}, '${value}', 1);`;
+            await sequelize.query(sql, { type: sequelize.QueryTypes.INSERT });
+        } else if (key == "telegram") {
+            let sql = `INSERT INTO contato VALUES ('${id}', ${forma++}, '${value}', 2);`;
+            await sequelize.query(sql, { type: sequelize.QueryTypes.INSERT });
+        } else if (key == "slack") {
+            let sql = `INSERT INTO contato VALUES ('${id}', ${forma++}, '${value}', 3);`;
+            await sequelize.query(sql, { type: sequelize.QueryTypes.INSERT });
+        }
+    });
+    return { status: "ok" };
+};
+
+module.exports = { updateDadosUsuario, criarFormasContato };

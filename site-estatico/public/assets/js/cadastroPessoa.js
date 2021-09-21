8cce63f994cf7ff67fe8dcb3906dc2ed03812787
nikolas-virionis <nikolas.virionis@bandtec.com.br>
2021-09-20 22:49:11 -0300

    logica de front end para cadastro proprio e de suas formas de contato personalizadas

--- site-estatico/public/assets/js/cadastroPessoa.js
+++ site-estatico/public/assets/js/cadastroPessoa.js
@@ -1,3 +1,4 @@
+let emailCadastro = document.querySelector("#inp-email");
 let contatos = document.querySelectorAll(".checkbox-contato");
 for (let contato of contatos) {
     let [label, input] = contato.children;
@@ -28,6 +29,7 @@ tokenInModal.addEventListener("keypress", (e) => {
     }
 });
 btnProsseguir.addEventListener("click", (e) => {
+    sessionStorage.setItem("email", emailInModal.value);
     axios
         .post("/usuario/verificacao", {
             email: emailInModal.value,
@@ -43,6 +45,7 @@ btnProsseguir.addEventListener("click", (e) => {
                 import("./modal.js").then(({ fecharModal }) =>
                     fecharModal("modal-verify-token")
                 );
+                emailCadastro.value = sessionStorage.getItem("email");
             } else console.log("Erro na verificação do usuario");
         });
 });
@@ -53,14 +56,108 @@ btnCancelar.addEventListener("click", async (e) => {
 
 var redes = ["whatsapp", "telegram", "slack"];
 
-for(var i = 0; i < redes.length; i++){
-    document.getElementById("contato-"+redes[i]).addEventListener("change", function(a){
-      if(this.checked){
-        document.getElementById("input-"+ this.id.slice(8,this.lenght)).style.visibility = 'visible';
-        document.getElementById("input-"+ this.id.slice(8,this.lenght)).style.width = "40%";
-      }else{
-        document.getElementById("input-"+ this.id.slice(8,this.lenght)).style.width = "0px";
-        document.getElementById("input-"+ this.id.slice(8,this.lenght)).style.visibility = "hidden";
-      }
-    })
+for (let rede of redes) {
+    document
+        .getElementById(`contato-${rede}`)
+        .addEventListener("change", function (a) {
+            if (this.checked) {
+                document.getElementById(`input-${rede}`).style.visibility =
+                    "visible";
+                document.getElementById(`input-${rede}`).style.width = "40%";
+            } else {
+                document.getElementById(`input-${rede}`).style.width = "0px";
+                document.getElementById(`input-${rede}`).style.visibility =
+                    "hidden";
+            }
+        });
+
+    document
+        .getElementById(`input-${rede}`)
+        .addEventListener("keypress", (e) => {
+            if (e.key == "Enter") {
+                e.preventDefault();
+            }
+        });
 }
+
+const nome = document.querySelector("#inp-nome");
+const senha = document.querySelector("#inp-senha");
+const confirmarSenha = document.querySelector("#inp-conf-senha");
+const btn = document.querySelector(".btn-geral");
+const form = document.querySelector("#form-cadastro");
+sessionStorage.removeItem("email");
+
+const validateEmail = (email) => {
+    const re =
+        /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email.toLowerCase());
+};
+
+nome.addEventListener("keypress", (e) => {
+    if (e.key == "Enter") {
+        e.preventDefault();
+        emailCadastro.focus();
+    }
+});
+emailCadastro.addEventListener("keypress", (e) => {
+    if (e.key == "Enter") {
+        e.preventDefault();
+        senha.focus();
+    }
+});
+senha.addEventListener("keypress", (e) => {
+    if (e.key == "Enter") {
+        e.preventDefault();
+        confirmarSenha.focus();
+    }
+});
+confirmarSenha.addEventListener("keypress", (e) => {
+    if (e.key == "Enter") {
+        e.preventDefault();
+        confirmarSenha.blur();
+    }
+});
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    if (senha.value !== confirmarSenha.value)
+        return console.log("Senhas diferentes");
+    let contatos = {};
+    if (document.getElementById("contato-whatsapp").checked)
+        contatos.whatsapp = document.getElementById("input-whatsapp").value;
+    if (document.getElementById("contato-telegram").checked)
+        contatos.telegram = document.getElementById("input-telegram").value;
+    if (document.getElementById("contato-slack").checked)
+        contatos.slack = document.getElementById("input-slack").value;
+    let id = JSON.parse(sessionStorage.getItem("id_usuario"));
+    axios
+        .post("/usuario/cadastro-final", {
+            id,
+            nome: nome.value,
+            email: emailCadastro.value,
+            senha: senha.value,
+            contatos,
+        })
+        .then((response) => {
+            if (response.data?.status == "ok") {
+                console.log("Cadastro final realizado com sucesso");
+                axios
+                    .post("/auth/usuario", {
+                        email: emailCadastro.value,
+                        senha: senha.value,
+                    })
+                    .then((resposta) => {
+                        sessionStorage.clear();
+                        if (resposta.data.status == "ok") {
+                            console.log("Usuario logado");
+                            let { status, ...user } = resposta.data;
+                            sessionStorage.setItem(
+                                "usuario",
+                                JSON.stringify(user)
+                            );
+                            window.location.href = "dashboard.html";
+                        }
+                    });
+            }
+        });
+});

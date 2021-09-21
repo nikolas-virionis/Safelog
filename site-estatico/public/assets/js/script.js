b555409a1d48c3a39bc0621577775d590249f472
nikolas-virionis <nikolas.virionis@bandtec.com.br>
2021-09-21 08:32:15 -0300

    redirecionamento automatico para um usuario não logado ao acessar alguma pagina de acesso restrito a usuarios logados

--- site-estatico/public/assets/js/script.js
+++ site-estatico/public/assets/js/script.js
@@ -2,3 +2,13 @@ const link = document.createElement("link");
 link.rel = "shortcut icon";
 link.href = "assets/img/logo/logo-icon-branco.png";
 document.getElementsByTagName("head")[0].appendChild(link);
+
+if (
+    window.location.pathname != "/login.html" &&
+    window.location.pathname != "/index.html" &&
+    window.location.pathname != "/" &&
+    !sessionStorage.usuario &&
+    !sessionStorage.staff
+) {
+    window.location.href = "login.html";
+}

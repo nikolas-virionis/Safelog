c429ed4992a03d902c8a02802e6f9098b8e60df5
nikolas-virionis <nikolas.virionis@bandtec.com.br>
2021-09-22 08:09:13 -0300

    relatório de incidentes, bd analytics, logica de front e back end

--- bd/BDAnalytics.sql
+++ bd/BDAnalytics.sql
@@ -66,7 +66,7 @@ CREATE TABLE usuario_maquina (
 
 CREATE TABLE analytics (
     id_dado int PRIMARY KEY AUTO_INCREMENT,
-    cpu_percent decimal(5, 2),
+    cpu decimal(5, 2),
     ram decimal(5, 2),
     disco decimal(5, 2),
     data_medicao datetime,

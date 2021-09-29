d27141d8872e2371248d0faf77917f4e3747281a
nikolas-virionis <nikolas.virionis@bandtec.com.br>
2021-09-29 08:22:30 -0300

    readaptação pagina de relatório (front e back end) ao novo bd +  novas funções por unidade de alerta ao gestor (em cometários)

--- site-estatico/public/assets/js/relatorio.js
+++ site-estatico/public/assets/js/relatorio.js
@@ -6,68 +6,134 @@ axios
     })
     .then((response) => {
         if (response.data?.status == "ok") {
-            let { status, response: maquinasIncidentes } = response.data;
-            let contagem = 0;
-            for(let i = 0; i < maquinasIncidentes.length; i++){
-                contagem += maquinasIncidentes[i].maquinas.length;
-            }
-            if(contagem > 0){
-                for (let {
-                    maquinas,
-                    limite_cpu,
-                    limite_ram,
-                    limite_disco,
-                } of maquinasIncidentes) {
-                    for (let maquina of maquinas) {
-                        Object.entries(maquina).forEach(([key, value]) => {
-                            if (
-                                key != "data_medicao" &&
-                                key != "nome" &&
-                                Number(value) >= eval(`limite_${key}`)
-                            ) {
-                                let tr = document.createElement("tr");
-                                let date = new Date(maquina.data_medicao);
-                                let data = `${date.toLocaleDateString("pt-BR")}
+            let { status, response: maquinas } = response.data;
+            // let contagem = 0;
+            // for (let i of maquinas) {
+            //     contagem += i.maquinas.length;
+            // }
+            if (maquinas.length > 0 && maquinas[0].length > 0) {
+                for (let maquina of maquinas) {
+                    for (let incidente of maquina) {
+                        let {
+                            tipo: tipoMedicao,
+                            nome,
+                            valor,
+                            unidade,
+                            data_medicao: dataMedicao,
+                            medicao_limite: limite,
+                        } = incidente;
+                        let [componente, tipo] = tipoMedicao.split("_");
+                        let tr = document.createElement("tr");
+                        let date = new Date(dataMedicao);
+                        let data = `${date.toLocaleDateString("pt-BR")}
                                 ${date.toTimeString().slice(0, 8)}`;
-                                let tbData = document.createElement("td");
-                                let tbNome = document.createElement("td");
-                                let tbComponente = document.createElement("td");
-                                let tbEstado = document.createElement("td");
-                                let tbMedicao = document.createElement("td");
-                                let tbOperacao = document.createElement("td");
-                                let alertarBtnLbl = document.createElement("i");
-                                let alertarBtn = document.createElement("button");
-                                alertarBtnLbl.classList = "fas fa-bell";
-                                alertarBtn.classList = "btn-nav-dash";
-                                alertarBtn.title = "Alertar Responsável";
-                                alertarBtn.appendChild(alertarBtnLbl);
-                                tbNome.innerHTML = maquina.nome;
-                                tbData.innerHTML = data;
-                                tbComponente.innerHTML = key.toUpperCase();
-                                tbMedicao.innerHTML = `${Number(value).toFixed(
-                                    2
-                                )}%`;
-                                tbOperacao.appendChild(alertarBtn);
-                                if (
-                                    Number(value) >=
-                                    eval(`limite_${key}`) * 0.6 + 38
-                                ) {
-                                    tbEstado.innerHTML = "Critico";
-                                    tr.style.color = "red";
-                                } else tbEstado.innerHTML = "Risco";
-                                tr.appendChild(tbData);
-                                tr.appendChild(tbNome);
-                                tr.appendChild(tbComponente);
-                                tr.appendChild(tbEstado);
-                                tr.appendChild(tbMedicao);
-                                tr.appendChild(tbOperacao);
-                                tabelaIncidentes.appendChild(tr);
-                            }
-                        });
+                        let tbData = document.createElement("td");
+                        let tbNome = document.createElement("td");
+                        let tbComponente = document.createElement("td");
+                        let tbTipo = document.createElement("td");
+                        let tbEstado = document.createElement("td");
+                        let tbMedicao = document.createElement("td");
+                        let tbOperacao = document.createElement("td");
+                        let alertarBtnLbl = document.createElement("i");
+                        let alertarBtn = document.createElement("button");
+                        alertarBtnLbl.classList = "fas fa-bell";
+                        alertarBtn.classList = "btn-nav-dash";
+                        alertarBtn.title = "Alertar Responsável";
+                        alertarBtn.appendChild(alertarBtnLbl);
+                        tbNome.innerHTML = nome;
+                        tbData.innerHTML = data;
+                        tbComponente.innerHTML = componente.toUpperCase();
+                        tbTipo.innerHTML =
+                            tipo.charAt(0).toUpperCase() + tipo.slice(1);
+                        tbMedicao.innerHTML = `${Number(valor)}${unidade}`;
+                        tbOperacao.appendChild(alertarBtn);
+                        // x * 0.6 + 38 => %
+                        // 0.65x + 28.5 => °C
+                        // 0.409x + 0.08 => ram livre
+                        // 0.26x + 21 => disco livre
+                        // 0.8x + 33.5 => cpu Mhz (%
+                        if (
+                            (tipo.toLowerCase() == "porcentagem" &&
+                                Number(valor) >= Number(limite) * 0.6 + 38) ||
+                            (tipo.toLowerCase() == "temperatura" &&
+                                Number(valor) >= Number(limite) * 0.65 + 28) ||
+                            (tipo.toLowerCase() == "livre" &&
+                                componente.toLowerCase() == "ram" &&
+                                Number(valor) <=
+                                    Number(limite) * 0.409 + 0.08) ||
+                            (tipo.toLowerCase() == "livre" &&
+                                componente.toLowerCase() == "disco" &&
+                                Number(valor) <= Number(limite) * 0.26 + 21) ||
+                            (tipo.toLowerCase() == "frequencia" &&
+                                Number(valor) >= Number(limite) * 0.8 + 33.5)
+                        ) {
+                            tbEstado.innerHTML = "Critico";
+                            tr.style.color = "red";
+                        } else {
+                            tbEstado.innerHTML = "Risco";
+                        }
+                        tr.appendChild(tbData);
+                        tr.appendChild(tbNome);
+                        tr.appendChild(tbComponente);
+                        tr.appendChild(tbTipo);
+                        tr.appendChild(tbEstado);
+                        tr.appendChild(tbMedicao);
+                        tr.appendChild(tbOperacao);
+                        tabelaIncidentes.appendChild(tr);
+
+                        // Object.entries(maquina).forEach(([key, value]) => {
+                        //     if (
+                        //         key != "data_medicao" &&
+                        //         key != "nome" &&
+                        //         Number(value) >= eval(`limite_${key}`)
+                        //     ) {
+                        //         let tr = document.createElement("tr");
+                        //         let date = new Date(maquina.data_medicao);
+                        //         let data = `${date.toLocaleDateString("pt-BR")}
+                        //         ${date.toTimeString().slice(0, 8)}`;
+                        //         let tbData = document.createElement("td");
+                        //         let tbNome = document.createElement("td");
+                        //         let tbComponente = document.createElement("td");
+                        //         let tbEstado = document.createElement("td");
+                        //         let tbMedicao = document.createElement("td");
+                        //         let tbOperacao = document.createElement("td");
+                        //         let alertarBtnLbl = document.createElement("i");
+                        //         let alertarBtn =
+                        //             document.createElement("button");
+                        //         alertarBtnLbl.classList = "fas fa-bell";
+                        //         alertarBtn.classList = "btn-nav-dash";
+                        //         alertarBtn.title = "Alertar Responsável";
+                        //         alertarBtn.appendChild(alertarBtnLbl);
+                        //         tbNome.innerHTML = maquina.nome;
+                        //         tbData.innerHTML = data;
+                        //         tbComponente.innerHTML = key.toUpperCase();
+                        //         tbMedicao.innerHTML = `${Number(value).toFixed(
+                        //             2
+                        //         )}%`;
+                        //         tbOperacao.appendChild(alertarBtn);
+                        //         if (
+                        //             Number(value) >=
+                        //             eval(`limite_${key}`) * 0.6 + 38
+                        //         ) {
+                        //             tbEstado.innerHTML = "Critico";
+                        //             tr.style.color = "red";
+                        //         } else tbEstado.innerHTML = "Risco";
+                        //         tr.appendChild(tbData);
+                        //         tr.appendChild(tbNome);
+                        //         tr.appendChild(tbComponente);
+                        //         tr.appendChild(tbEstado);
+                        //         tr.appendChild(tbMedicao);
+                        //         tr.appendChild(tbOperacao);
+                        //         tabelaIncidentes.appendChild(tr);
+                        //     }
+                        // });
                     }
                 }
-            }else{
-                mostrarAlerta("Ainda não há nenhum registro", "info");
+            } else {
+                mostrarAlerta(
+                    "Ainda não há registros de incidentes em maquinas de seu acesso",
+                    "info"
+                );
             }
         } else {
             console.error(response.data.msg);
--- site-estatico/routes/medicao.js
+++ site-estatico/routes/medicao.js
@@ -10,33 +10,23 @@ router.post("/relatorio-incidentes", async (req, res, next) => {
             status: "erro",
             msg: "Body não fornecido na requisição",
         });
-    let sql = `SELECT id_maquina, limite_cpu, limite_ram, limite_disco FROM maquina JOIN usuario_maquina ON id_maquina = fk_maquina and fk_usuario = ${id}`;
+    let sql = `SELECT id_maquina FROM maquina JOIN usuario_maquina ON id_maquina = fk_maquina and fk_usuario = ${id}`;
 
     await sequelize
         .query(sql, { type: sequelize.QueryTypes.SELECT })
         .then(async (response) => {
             let incidentes = [];
-            for (let {
-                id_maquina,
-                limite_cpu,
-                limite_ram,
-                limite_disco,
-            } of response) {
-                let incidente = `SELECT data_medicao, cpu, ram, disco, maquina.nome FROM usuario_maquina JOIN maquina ON usuario_maquina.fk_maquina = id_maquina and fk_usuario = ${id} JOIN analytics ON analytics.fk_maquina = maquina.id_maquina WHERE cpu >= ${limite_cpu} and id_maquina = '${id_maquina}' or ram >= ${limite_ram} and id_maquina = '${id_maquina}' or disco >= ${limite_disco} and id_maquina = '${id_maquina}' ORDER BY data_medicao desc LIMIT 15;`;
+            for (let { id_maquina } of response) {
+                let incidente = `SELECT data_medicao, valor, unidade, medicao_limite, tipo_medicao.tipo, maquina.nome FROM maquina JOIN categoria_medicao ON id_maquina = fk_maquina JOIN tipo_medicao ON id_tipo_medicao = fk_tipo_medicao JOIN medicao ON fk_categoria_medicao = id_categoria_medicao and (IF(tipo_medicao.tipo like '%livre', medicao.valor <= categoria_medicao.medicao_limite, medicao.valor >= categoria_medicao.medicao_limite)) and id_maquina = '${id_maquina}' GROUP BY valor, tipo ORDER BY data_medicao DESC;`;
                 await sequelize
                     .query(incidente, {
                         type: sequelize.QueryTypes.SELECT,
                     })
                     .then((result) => {
-                        incidentes.push({
-                            maquinas: [...result],
-                            limite_cpu,
-                            limite_ram,
-                            limite_disco,
-                        });
+                        incidentes.push(result);
                     });
             }
-            res.json({ status: "ok", response: incidentes });
+            return res.json({ status: "ok", response: incidentes });
         })
         .catch((err) => res.json({ status: "erro", msg: err }));
 });

e5381d8730d862f27bacbb5f2b46cd07416c1bdb
nikolas-virionis <nikolas.virionis@bandtec.com.br>
2021-10-07 17:52:49 -0300

    Co-authored-by: Lucas Mesquita de Souza <lucasmsouza24@users.noreply.github.com>

--- bd/export/create-insert.py
+++ bd/export/create-insert.py
@@ -1,1 +1,30 @@
 # script para tentar transformar uma query de select em um txt de um insert similar
+import mysql.connector as sql
+import pandas as pd
+import pyperclip
+
+db_connection = sql.connect(host='localhost', database='safelog',
+                            user='root', password='DigitalSchool1$')
+medicoes = pd.read_sql(f"SELECT * FROM medicao WHERE fk_categoria_medicao > 56",
+                       con=db_connection)
+categorias = pd.read_sql(
+    f"SELECT * FROM categoria_medicao WHERE id_categoria_medicao > 56",
+    con=db_connection)
+print(categorias)
+limites = categorias["medicao_limite"]
+maquinas = categorias["fk_maquina"]
+tipos_med = categorias["fk_tipo_medicao"]
+valores = medicoes["valor"]
+tipos = medicoes["tipo"]
+datas = medicoes["data_medicao"]
+fks = medicoes["fk_categoria_medicao"]
+
+insert_sql = ""
+insert_sql += "INSERT INTO maquina VALUES ('0C:9D:92:C6:CC:19', 'DESKTOP1', MD5('ExSenha1'), '203783731'); \n"
+insert_sql += "INSERT INTO usuario_maquina(responsavel, fk_usuario, fk_maquina) VALUES ('s', 13, '0C:9D:92:C6:CC:19'); \n"
+for i, j in enumerate(limites):
+    insert_sql += f"INSERT INTO categoria_medicao(medicao_limite, fk_maquina, fk_tipo_medicao) VALUES ({limites[i]}, '{maquinas[i]}', {tipos_med[i]}); \n"
+for i, j in enumerate(valores):
+    insert_sql += f"INSERT INTO medicao(valor, tipo, data_medicao, fk_categoria_medicao) VALUES ({valores[i]}, '{tipos[i]}', '{datas[i]}', {fks[i]}); \n"
+print(insert_sql)
+pyperclip.copy(insert_sql)

e2225d8f6fa526dddea5dad2258862147045883c
nikolas-virionis <nikolas.virionis@bandtec.com.br>
2021-10-07 07:51:18 -0300

    main.py criando pastas e csv de maquinas aleatórias e capturando preferencias de maquina e empresa

---
+++ bd/export/create-insert.py
@@ -0,0 +1,1 @@
+# script para tentar transformar uma query de select em um txt de um insert similar
--- bd/export/main.py
+++ bd/export/main.py
@@ -1,33 +1,56 @@
 import mysql.connector as sql
-import random
 import pandas as pd
 import sys
+import os
 
 db_connection = sql.connect(host='localhost', database='safelog',
                             user='safelog_dev', password='Safe_Log371$')
 
 
-def df_to_csv(tb):
+def df_to_csv(tb, emp, name):
     df = pd.read_sql(f"SELECT * FROM v_analytics WHERE fk_maquina = '{tb}'",
                      con=db_connection)
+    emp = emp.strip().replace(" ", "_")
+    print(os.path.exists(f"./bd/export/csv/{emp}"), emp == 'NYSE_MARKET,_INC')
+    if not os.path.exists(f"./bd/export/csv/{emp}"):
+        os.mkdir(f"./bd/export/csv/{emp}")
     print(df)
-    df.to_csv(f"./bd/export/amostra{tb.replace(':', '')}.csv", index=False)
+    df.to_csv(
+        f"./bd/export/csv/{emp}/amostra{name.capitalize()}.csv", index=True)
 
 
 if len(sys.argv) == 1:
-    tb = random.choice(pd.read_sql("SELECT * from maquina",
-                                   con=db_connection)["id_maquina"])
-    print(tb)
-    df_to_csv(tb)
+    select = pd.read_sql("SELECT id_maquina, nome, fk_empresa FROM maquina ORDER BY RAND() LIMIT 1",
+                         con=db_connection)
+    tb, name, fk_empresa = select["id_maquina"][0], select["nome"][0], select["fk_empresa"][0]
+    print(tb, name, fk_empresa)
+    emp = pd.read_sql(
+        f"SELECT nome from empresa WHERE id_empresa = '{fk_empresa}'", con=db_connection)["nome"][0]
+    print("random: ", tb, emp, name)
+    df_to_csv(tb, emp, name)
+# dict(zip(new_keys,old_dict.values()))
 elif sys.argv[1] == "maquina":
-    tb = sys.argv[2]
-    df_to_csv(tb)
+    tb = sys.argv[2].lower()
+    select = pd.read_sql(
+        f"SELECT empresa.nome from maquina JOIN empresa ON fk_empresa = id_empresa AND id_maquina = '{tb}'", con=db_connection)
+    emp = select["nome"][0].strip()
+    print("\n\n", emp, emp == "NYSE MARKET, INC", "\n\n")
+    select = pd.read_sql(
+        f"SELECT nome from maquina WHERE id_maquina = '{tb}'", con=db_connection)
+    name = select["nome"][0]
+    df_to_csv(tb, emp, name)
 
 elif sys.argv[1] == "empresa":
     emp = sys.argv[2]
+    print("\n\n", emp, emp == "NYSE MARKET, INC", "\n\n")
     print(
-        f"SELECT id_maquina from maquina WHERE fk_empresa = (SELECT id_empresa FROM empresa where nome = '{emp}')")
-    tbs = pd.read_sql(f"SELECT id_maquina from maquina WHERE fk_empresa = (SELECT id_empresa FROM empresa where nome = '{emp}')",
-                      con=db_connection)["id_maquina"]
-    for tb in tbs:
-        df_to_csv(tb)
+        f"SELECT id_maquina, nome from maquina WHERE fk_empresa = (SELECT id_empresa FROM empresa where nome like '{emp}')")
+    select = pd.read_sql(f"SELECT id_maquina, nome from maquina WHERE fk_empresa = (SELECT id_empresa FROM empresa where nome like '{emp}')",
+                         con=db_connection)
+    id_maquina = select["id_maquina"]
+    nome = select["nome"]
+    print("\n\n", id_maquina, nome, "\n\n")
+    for i, j in enumerate(id_maquina):
+        tb = id_maquina[i]
+        name = nome[i]
+        df_to_csv(tb, emp, name)

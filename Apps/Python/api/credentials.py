3124835175159cfe8ee161f6dfffce17099c94e4
nikolas-virionis <nikolas.virionis@bandtec.com.br>
2021-10-09 19:18:20 -0300

    api python, insert bd, modelo operacional => pivot funcional

--- Apps/Python/api/credentials.py
+++ Apps/Python/api/credentials.py
@@ -1,2 +1,7 @@
+import mysql.connector as sql
+
 usr = 'root'
 pswd = 'DigitalSchool1$'
+
+db_connection = sql.connect(host='localhost', database='safelog',
+                            user=usr, password=pswd)
--- Apps/Python/api/insert-db-aut.py
+++ Apps/Python/api/insert-db-aut.py
@@ -1,21 +1,36 @@
+import pandas as pd
 import psutil
 import time
-# from functools import reduce
-from connectdb import *
+from connectdb import insert_db
+from credentials import db_connection
+from getmac import get_mac_address as mac_addr
 
-print('='*10, 'INÍCIO DAS MEDIÇÕES', '='*10)
-print('-'*10, 'Ctrl+C para parar', '-'*10, '\n')
+preferences = pd.read_sql(
+    f"SELECT tipo_medicao.tipo FROM categoria_medicao INNER JOIN tipo_medicao ON fk_tipo_medicao = id_tipo_medicao WHERE fk_maquina = '{mac_addr()}'", con=db_connection)
 
 try:
     while True:
-        ram = (psutil.virtual_memory().percent)
-        cpu_percent = psutil.cpu_percent()
-        disco = psutil.disk_usage('/').percent
         data_medicao = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-        print("mem_used:", float('{0:.2f}'.format(ram)), "|cpu:", float(
-            '{0:.2f}'.format(cpu_percent)), "| date:", data_medicao)
-        insert_db([float(f'{cpu_percent:.2f}'), float(
-            f'{ram:.2f}'), float(f'{disco:.2f}')])
-        time.sleep(3)
+        medicao = 0
+        dados = [data_medicao]
+        for tipo in preferences["tipo"]:
+            if tipo == "cpu_frequencia":
+                medicao = round(psutil.cpu_freq().current *
+                                100 / psutil.cpu_freq().max, 2)
+            elif tipo == "cpu_porcentagem":
+                medicao = round(psutil.cpu_percent(), 2)
+            elif tipo == "ram_porcentagem":
+                medicao = round(psutil.virtual_memory().percent, 2)
+            elif tipo == "ram_livre":
+                medicao = round(psutil.virtual_memory().free / 1073741824, 2)
+            elif tipo == "disco_livre":
+                medicao = round(psutil.disk_usage('/').free / 1073741824, 2)
+            elif tipo == "disco_porcentagem":
+                medicao = round(psutil.disk_usage('/').percent, 2)
+            else:
+                continue
+            dados.append({"tipo": tipo, "medicao": medicao})
+        insert_db(dados)
+        time.sleep(30)
 except KeyboardInterrupt:
     pass

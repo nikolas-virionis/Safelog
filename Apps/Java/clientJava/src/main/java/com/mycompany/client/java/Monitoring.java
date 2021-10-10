f56850c9b744b4e1368f064ab89efa5d209b299a
João Pedro Santos <joao.soliveira@gmail.com>
2021-10-09 22:01:36 -0300

    Entendendo e Comentando API Looca

--- Apps/Java/clientJava/src/main/java/com/mycompany/client/java/Monitoring.java
+++ Apps/Java/clientJava/src/main/java/com/mycompany/client/java/Monitoring.java
@@ -9,22 +9,28 @@ import oshi.hardware.NetworkIF;
 import oshi.hardware.CentralProcessor;
 import oshi.hardware.HardwareAbstractionLayer;
 
+// Essa Classe tem como herança a Classe Looca, o comando "extends" pega os valores atributos
+// da classe Looca para serem utilizados de forma simples na classe "Monitoring"
 public class Monitoring extends Looca {
 
+    // Dentro deste Método é chamada a Classe "SystemInfo" que retorna as informações do getSystemHardware() 
     public static HardwareAbstractionLayer getSystemHardware() {
         SystemInfo sys = new SystemInfo();
         return sys.getHardware();
     }
 
+    // Criação  de uma lista com a quantidade de placas de redes e retorno desses dados com o getSystemHardware()
     public static String getMacAddress() {
         List<NetworkIF> netIfs = getSystemHardware().getNetworkIFs();
         return netIfs.get(0).getMacaddr();
     }
 
+    // Formatação dos dados de Temperatura
     public Double getTemp() {
         return Math.round(super.getTemperatura().getTemperatura() * 100) / 100d;
     }
 
+    // Formatação dos dados do Clock para porcentagem
     public Double getClockCPU() {
         CentralProcessor cpu = getSystemHardware().getProcessor();
         Long maxFreq = cpu.getMaxFreq();
@@ -32,23 +38,28 @@ public class Monitoring extends Looca {
         return Math.round((freq * 10000) / (maxFreq)) / 100d;
     }
 
+    // Formatação dos dados de Uso da CPU para porcentagem
     public Double getUsoCPU() {
         return Math.round(super.getProcessador().getUso() * 100) / 100d;
     }
 
+    // Formatação dos dados de uso da Ram para GB para porcentagem
     public Double getUsoRAMGb() {
         return Math.round(super.getMemoria().getEmUso() * 100 / 1_073_741_824) / 100d;
     }
 
+    // Formatação dos dados de Ram livre para GB e porcentagem
     public Double getFreeRAMGb() {
         return Math.round(
                 super.getMemoria().getDisponivel() * 100 / 1_073_741_824) / 100d;
     }
 
+    // Formatação dos dados do total de Ram para GB e porcentagem 
     public Double getTotalRAMGb() {
         return Math.round(super.getMemoria().getTotal() * 100 / 1_073_741_824) / 100d;
     }
 
+    // Formatação dos dados do uso de Disco para GB e porcentagem
     public Double getUsoDiscoGb() {
         // System.out.println("-".repeat(30));
         // System.out.println(super.getGrupoDeDiscos().getDiscos().get(0));
@@ -62,11 +73,13 @@ public class Monitoring extends Looca {
                 / 100d;
     }
 
+    // Formatação dos dados do total de Disco para GB e porcentagem
     public Double getTotalDiscoGb() {
         return Math.round(super.getGrupoDeDiscos().getDiscos().get(0).
                 getTamanho() * 100 / 1_073_741_824) / 100d;
     }
 
+    // Formatação dos dados de Disco livre para GB e porcentagem
     public Double getFreeDiscoGb() {
         return Math.round((getTotalDiscoGb() - getUsoDiscoGb()) * 100) / 100d;
     }

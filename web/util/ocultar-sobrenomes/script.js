99b04a520a51c97a965a89814b830faa31fa444d
nikolas-virionis <nikolas.virionis@bandtec.com.br>
2021-10-11 13:06:11 -0300

    delete logica de ocultar sobrenomes de supervisores

--- web/util/ocultar-sobrenomes/script.js
+++
@@ -1,9 +0,0 @@
-const criptografarNome = (nome) => {
-    let nomes = nome.split(" ");
-    let parteOculta = nomes.slice(1);
-    let sobrenome = parteOculta.map((element) => "*".repeat(element.length));
-    return `${
-        nomes[0].charAt(0).toUpperCase() + nomes[0].slice(1)
-    } ${sobrenome.join(" ")}`;
-};
-module.exports = { criptografarNome };

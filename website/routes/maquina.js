7ab665be7d9e328313d8a043ad0f18884e1fd15d
nikolas-virionis <nikolas.virionis@bandtec.com.br>
2021-10-19 17:21:19 -0300

    correção rota lista usuários: não inclui mais o responsável

--- website/routes/maquina.js
+++ website/routes/maquina.js
@@ -289,7 +289,7 @@ router.post("/lista-usuarios", async (req, res) => {
             msg: "Body não fornecido na requisição"
         });
 
-    let sql = `SELECT id_usuario, nome, email FROM usuario JOIN usuario_maquina ON id_usuario = fk_usuario AND fk_maquina = ${id}`;
+    let sql = `SELECT id_usuario, nome, email FROM usuario JOIN usuario_maquina ON id_usuario = fk_usuario AND fk_maquina = ${id} AND responsavel = 'n'`;
 
     await sequelize
         .query(sql, {type: sequelize.QueryTypes.SELECT})

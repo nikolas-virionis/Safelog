14c28a8d5a194575454a7d0c6da750211af749e9
nikolas-virionis <nikolas.virionis@bandtec.com.br>
2021-10-19 17:26:39 -0300

    melhorias no código do gerenciamento de acessos a maquina

--- website/public/assets/js/acessoMaquina.js
+++ website/public/assets/js/acessoMaquina.js
@@ -1,72 +1,87 @@
 const urlParams = new URLSearchParams(window.location.search);
 const pkMaquina = Number(urlParams.get("pk_maquina"));
 
-axios.post("/maquina/dados", {
-    maquina: pkMaquina
-}).then(({data: {status, msg}}) => {
-    document.querySelector(".titulo-acesso").innerHTML = msg.nome
-});
-
-axios.post("/maquina/lista-usuarios", { 
-    id: pkMaquina 
-}).then(({data: {status, msg}}) => {
-    
-    msg.forEach(registro => {
-        let tr = document.createElement("tr");
-        let tdNome = document.createElement("td");
-        let tdEmail = document.createElement("td");
-        let tdBotoes = document.createElement("td");
-        
-        let btnStar = document.createElement("button");
-        btnStar.classList.add("btn-nav-dash-yellow");
-        let btnTimes = document.createElement("button");
-        btnTimes.classList.add("btn-nav-dash-red");
-        
-        let starLbl = document.createElement("i");
-        starLbl.classList = "fas fa-star";
-        let timesLbl = document.createElement("i");
-        timesLbl.classList = "fas fa-times";
-
-        tdNome.innerHTML = registro.nome;
-        tdEmail.innerHTML = registro.email;
-
-        btnStar.appendChild(starLbl);
-        btnTimes.appendChild(timesLbl);
-
-        tdBotoes.appendChild(btnStar);
-        tdBotoes.appendChild(btnTimes);
-
-        tr.appendChild(tdNome);
-        tr.appendChild(tdEmail);
-        tr.appendChild(tdBotoes);
-
-        document.querySelector("#tblAcessoMaq").appendChild(tr);
-        
-        
-        
-        btnTimes.addEventListener("click", (e) => {
-            let confirmar = confirm(`Você realmente deseja tirar o acesso de ${registro.nome}`)
-            if(confirmar){
-                axios.post("/usuario/remocao-acesso", {
-                    id: registro.id_usuario,
-                    maquina: pkMaquina
-                }).then((e)=>{
-                    mostrarAlerta("Acesso removido com sucesso", "success");
-                    setTimeout(()=>{ window.location.reload() } ,1000 );
-                });
-            }
-        });
+axios
+    .post("/maquina/dados", {
+        maquina: pkMaquina
+    })
+    .then(({data: {status, msg}}) => {
+        if (status === "ok") {
+            document.querySelector(".titulo-acesso").innerHTML = msg.nome;
+        } else {
+            console.error("Erro: ", msg);
+        }
     });
 
-});
+axios
+    .post("/maquina/lista-usuarios", {
+        id: pkMaquina
+    })
+    .then(({data: {status, msg}}) => {
+        if (status === "ok") {
+            let tbUsuarios = document.querySelector("#tblAcessoMaq");
+            msg.forEach(({nome, email, id_usuario}) => {
+                let tr = document.createElement("tr");
+                let tdNome = document.createElement("td");
+                let tdEmail = document.createElement("td");
+                let tdOperacoes = document.createElement("td");
 
+                let btnResp = document.createElement("button");
+                btnResp.classList.add("btn-nav-dash-yellow");
+                let btnDelete = document.createElement("button");
+                btnDelete.classList.add("btn-nav-dash-red");
+
+                let lblResp = document.createElement("i");
+                lblResp.classList = "fas fa-star";
+                let lblDelete = document.createElement("i");
+                lblDelete.classList = "fas fa-times";
+
+                btnResp.appendChild(lblResp);
+                btnDelete.appendChild(lblDelete);
+
+                tdOperacoes.appendChild(btnResp);
+                tdOperacoes.appendChild(btnDelete);
+
+                tdNome.innerHTML = nome;
+                tdEmail.innerHTML = email;
+
+                tr.appendChild(tdNome);
+                tr.appendChild(tdEmail);
+                tr.appendChild(tdOperacoes);
+
+                tbUsuarios.appendChild(tr);
+
+                btnDelete.addEventListener("click", e => {
+                    let confirmar = confirm(
+                        `Você realmente deseja tirar o acesso de ${nome}`
+                    );
+                    if (confirmar) {
+                        axios
+                            .post("/usuario/remocao-acesso", {
+                                id: id_usuario,
+                                maquina: pkMaquina
+                            })
+                            .then(({data: {status, msg}}) => {
+                                if (status === "ok") {
+                                    mostrarAlerta(msg, "success");
+                                    setTimeout(() => {
+                                        window.location.reload();
+                                    }, 2000);
+                                }
+                            });
+                    }
+                });
+            });
+        } else {
+            mostrarAlerta("Erro no gerenciamento de usuario", "danger");
+        }
+    });
 
 document.querySelector("#btnAddUser").addEventListener("click", () => {
     import("./modal.js").then(({abrirModal}) =>
         abrirModal("modal-invite-user")
     );
 });
-
 
 const btnCancelar = document.querySelector("#btn-cancelar-modal");
 

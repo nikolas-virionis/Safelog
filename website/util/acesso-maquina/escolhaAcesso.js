9548fce25341adf449f2ae32cef5c70187d4c90e
LucasTeixeira793 <lucas.teixeira@bandtec.com.br>
2021-10-22 09:54:53 -0300

    meu commit de correção

--- website/util/acesso-maquina/escolhaAcesso.js
+++ website/util/acesso-maquina/escolhaAcesso.js
@@ -26,7 +26,7 @@ const escolhaResp = async (id, maquina) => {
         });
 };
 
-const escolhaAuto = (id, maquina) => {
+const escolhaAuto = async (id, maquina) => {
     let updateResponsavel = `UPDATE usuario_maquina SET responsavel = 's' WHERE fk_maquina = ${maquina} AND fk_usuario = ${id}`;
 
     return await sequelize
@@ -41,7 +41,7 @@ const escolhaAuto = (id, maquina) => {
         });
 };
 
-const conviteResp = (id, maquina, tipo) => {
+const conviteResp = async (id, maquina, tipo) => {
     let sql = `SELECT g.nome as nomeGestor, g.email, a.nome, pk_maquina, maquina.nome as nomeMaquina FROM usuario as a JOIN usuario as g ON a.fk_supervisor = g.id_usuario JOIN usuario_maquina ON a.id_usuario = fk_usuario AND a.id_usuario = ${id} JOIN maquina ON fk_maquina = id_maquina WHERE id_maquina = '${maquina}' OR pk_maquina = ${maquina}`;
     await sequelize
         .query(sql, {

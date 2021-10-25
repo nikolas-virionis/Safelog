478d85b5b38431656f053d850762a08456c3a095
nikolas-virionis <nikolas.virionis@bandtec.com.br>
2021-10-24 21:09:20 -0300

    link para perfil na sidebar: foto e nome

--- website/public/assets/js/sidebar.js
+++ website/public/assets/js/sidebar.js
@@ -16,6 +16,16 @@ let homeSection = document.querySelector(".home-section");
 homeSection.addEventListener("click", () => sidebar.classList.add("close"));
 
 const nomeUser = document.querySelector(".profile_name");
+const profilePic = document.querySelector(".profilePic");
+
+[nomeUser, profilePic].forEach(el => {
+    el.addEventListener("click", (evt => {
+        redirectTo("perfil");
+    }))
+    el.classList.add("clickble");
+    el.title = "Perfil";
+})
+
 nomeUser.innerText =
     JSON.parse(sessionStorage.getItem("usuario"))?.nome?.split(" ").shift() ??
     JSON.parse(sessionStorage.getItem("staff"))?.nome?.split(" ").shift() ??
@@ -25,3 +35,7 @@ document.querySelector(".icon-exit").addEventListener("click", function () {
     sessionStorage.clear();
     window.location.href = "/";
 });
+
+const redirectTo = (url) => {
+    window.location.href = "./" + url;
+}

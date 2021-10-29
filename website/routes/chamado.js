3883c48dcd9379b7c98a06b5f05ae59ad10586a1
nikolas-virionis <nikolas.virionis@bandtec.com.br>
2021-10-29 17:57:44 -0300

    refactor criação de chamado => checa chamados abertos sobre mesma métrica + permite recriação, novo chamado, mudando a eficacia das soluções anteriores

--- website/routes/chamado.js
+++ website/routes/chamado.js
@@ -4,73 +4,106 @@ let sequelize = require("../models").sequelize;
 const {abrirChamado} = require("../util/chamado/abertura");
 
 router.post("/criar", async (req, res) => {
-    let {titulo, desc, prioridade, idCategoriaMedicao, idUsuario} = req.body;
+    let {
+        titulo,
+        desc,
+        prioridade,
+        idCategoriaMedicao,
+        idUsuario,
+        eficaciaSolucoes
+    } = req.body;
     if (!req.body) {
         return res.json({
             status: "erro",
             msg: "Body não fornecido na requisição"
         });
-    } else {
-        // verificando se usuario tem acesso à máquina
-        const sqlUsuarioTemAcesso = `SELECT * FROM usuario_maquina WHERE fk_usuario = ${idUsuario} AND fk_maquina = 
-        (SELECT fk_maquina FROM categoria_medicao WHERE id_categoria_medicao = ${idCategoriaMedicao})`;
+    }
+    const chamadoJaAberto = `SELECT status_chamado FROM chamado WHERE fk_categoria_medicao = ${idCategoriaMedicao}`;
+    await sequelize
+        .query(chamadoJaAberto, {type: sequelize.QueryTypes.SELECT})
+        .then(async ([status]) => {
+            if (status?.status_chamado == "aberto") {
+                res.json({
+                    status: "alerta",
+                    msg: "Chamado já aberto para essa métrica"
+                });
+            } else {
+                // verificando se usuario tem acesso à máquina
+                const sqlUsuarioTemAcesso = `SELECT * FROM usuario_maquina WHERE fk_usuario = ${idUsuario} AND fk_maquina = (SELECT fk_maquina FROM categoria_medicao WHERE id_categoria_medicao = ${idCategoriaMedicao})`;
 
-        await sequelize
-            .query(sqlUsuarioTemAcesso, {type: sequelize.QueryTypes.SELECT})
-            .then(response => {
-                if (response.length > 0) {
-                    // usuario tem acesso
-                    abrirChamado(
-                        titulo,
-                        desc,
-                        idUsuario,
-                        idCategoriaMedicao,
-                        prioridade
-                    )
-                        .then(resposta => {
-                            res.json(resposta);
-                        })
-                        .catch(err => {
-                            res.json({status: "erro1", msg: err});
-                        });
-                } else {
-                    const sqlGestorTemAcesso = `SELECT * FROM usuario_maquina JOIN usuario ON fk_usuario = id_usuario AND fk_supervisor = ${idUsuario} AND fk_maquina = (SELECT fk_maquina FROM categoria_medicao WHERE id_categoria_medicao = ${idCategoriaMedicao})`;
-                    sequelize
-                        .query(sqlGestorTemAcesso, {
-                            type: sequelize.QueryTypes.SELECT
-                        })
-                        .then(async ([response]) => {
-                            if (response) {
-                                abrirChamado(
-                                    titulo,
-                                    desc,
-                                    idUsuario,
-                                    idCategoriaMedicao,
-                                    prioridade
-                                )
-                                    .then(resposta => {
-                                        res.json(resposta);
+                await sequelize
+                    .query(sqlUsuarioTemAcesso, {
+                        type: sequelize.QueryTypes.SELECT
+                    })
+                    .then(async ([response]) => {
+                        if (response) {
+                            // usuario tem acesso
+                            if (eficaciaSolucoes) {
+                                const updateEficaciaSolucoes = `UPDATE solucao SET eficacia = '${eficaciaSolucoes}' WHERE eficacia = 'total' AND fk_chamado = (SELECT id_chamado FROM chamado WHERE fk_categoria_medicao = ${idCategoriaMedicao})`;
+
+                                await sequelize
+                                    .query(updateEficaciaSolucoes, {
+                                        type: sequelize.QueryTypes.UPDATE
                                     })
                                     .catch(err => {
-                                        res.json({status2: "erro", msg: err});
+                                        res.json({status: "erro1", msg: err});
                                     });
-                            } else {
-                                // usuario não tem acesso
-                                res.json({
-                                    status: "alerta",
-                                    msg: "Usuário não tem acesso à máquina"
-                                });
                             }
+                            abrirChamado(
+                                titulo,
+                                desc,
+                                idUsuario,
+                                idCategoriaMedicao,
+                                prioridade
+                            )
+                                .then(resposta => {
+                                    res.json(resposta);
+                                })
+                                .catch(err => {
+                                    res.json({status: "erro1", msg: err});
+                                });
+                        } else {
+                            const sqlGestorTemAcesso = `SELECT * FROM usuario_maquina JOIN usuario ON fk_usuario = id_usuario AND fk_supervisor = ${idUsuario} AND fk_maquina = (SELECT fk_maquina FROM categoria_medicao WHERE id_categoria_medicao = ${idCategoriaMedicao})`;
+                            sequelize
+                                .query(sqlGestorTemAcesso, {
+                                    type: sequelize.QueryTypes.SELECT
+                                })
+                                .then(async ([response]) => {
+                                    if (response) {
+                                        abrirChamado(
+                                            titulo,
+                                            desc,
+                                            idUsuario,
+                                            idCategoriaMedicao,
+                                            prioridade
+                                        )
+                                            .then(resposta => {
+                                                res.json(resposta);
+                                            })
+                                            .catch(err => {
+                                                res.json({
+                                                    status2: "erro",
+                                                    msg: err
+                                                });
+                                            });
+                                    } else {
+                                        // usuario não tem acesso
+                                        res.json({
+                                            status: "alerta",
+                                            msg: "Usuário não tem acesso à máquina"
+                                        });
+                                    }
+                                });
+                        }
+                    })
+                    .catch(err => {
+                        return res.json({
+                            status: "erro3",
+                            msg: err
                         });
-                }
-            })
-            .catch(err => {
-                return res.json({
-                    status: "erro3",
-                    msg: err
-                });
-            });
-    }
+                    });
+            }
+        });
 });
 
 module.exports = router;

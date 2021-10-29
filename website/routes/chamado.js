399e91dd9d86c3325a506cda699f34335e4c7ffd
nikolas-virionis <nikolas.virionis@bandtec.com.br>
2021-10-29 07:05:50 -0300

    mini refactor na criação de chamado: inclusão do gestor e reutilização de código pela /util/chamado

--- website/routes/chamado.js
+++ website/routes/chamado.js
@@ -1,57 +1,76 @@
 let express = require("express");
 let router = express.Router();
 let sequelize = require("../models").sequelize;
+const {abrirChamado} = require("../util/chamado/abertura");
 
 router.post("/criar", async (req, res) => {
     let {titulo, desc, prioridade, idCategoriaMedicao, idUsuario} = req.body;
     if (!req.body) {
         return res.json({
-            status: "erro", 
+            status: "erro",
             msg: "Body não fornecido na requisição"
         });
     } else {
-
-        // verificando se usuario tem acesso à máquina    
+        // verificando se usuario tem acesso à máquina
         const sqlUsuarioTemAcesso = `SELECT * FROM usuario_maquina WHERE fk_usuario = ${idUsuario} AND fk_maquina = 
         (SELECT fk_maquina FROM categoria_medicao WHERE id_categoria_medicao = ${idCategoriaMedicao})`;
 
-        await sequelize.query(sqlUsuarioTemAcesso, {type: sequelize.QueryTypes.SELECT})
-        .then(async (response) => {
-
-            // usuario tem acesso
-            if (response.length > 0) {
-                let sqlCriaChamado = `INSERT INTO chamado(titulo, descricao, data_abertura, status_chamado, prioridade, fk_categoria_medicao, fk_usuario) VALUES 
-                ('${titulo}', '${desc}', NOW(), 'aberto', '${prioridade}', ${idCategoriaMedicao}, ${idUsuario})`;
-
-                sequelize.query(sqlCriaChamado, {type: sequelize.QueryTypes.INSERT})
-                .then(response => {
-                    return res.json({
-                        status: "ok",
-                        msg: "chamado aberto",
-                        res: response
-                    });
-                })
-                .catch(err => {
-                    return res.json({
-                        status: "erro", 
-                        msg: err
-                    })
-                })
-            } else {
-                // usuario não tem acesso 
-                res.json({
-                    status: "alerta",
-                    msg: "usuário não tem acesso à máquina"
-                })
-            }
-        }) 
-        .catch(err => {
-            return res.json({
-                status: "erro", 
-                msg: err
+        await sequelize
+            .query(sqlUsuarioTemAcesso, {type: sequelize.QueryTypes.SELECT})
+            .then(response => {
+                if (response.length > 0) {
+                    // usuario tem acesso
+                    abrirChamado(
+                        titulo,
+                        desc,
+                        idUsuario,
+                        idCategoriaMedicao,
+                        prioridade
+                    )
+                        .then(resposta => {
+                            res.json(resposta);
+                        })
+                        .catch(err => {
+                            res.json({status: "erro1", msg: err});
+                        });
+                } else {
+                    const sqlGestorTemAcesso = `SELECT * FROM usuario_maquina JOIN usuario ON fk_usuario = id_usuario AND fk_supervisor = ${idUsuario} AND fk_maquina = (SELECT fk_maquina FROM categoria_medicao WHERE id_categoria_medicao = ${idCategoriaMedicao})`;
+                    sequelize
+                        .query(sqlGestorTemAcesso, {
+                            type: sequelize.QueryTypes.SELECT
+                        })
+                        .then(async ([response]) => {
+                            if (response) {
+                                abrirChamado(
+                                    titulo,
+                                    desc,
+                                    idUsuario,
+                                    idCategoriaMedicao,
+                                    prioridade
+                                )
+                                    .then(resposta => {
+                                        res.json(resposta);
+                                    })
+                                    .catch(err => {
+                                        res.json({status2: "erro", msg: err});
+                                    });
+                            } else {
+                                // usuario não tem acesso
+                                res.json({
+                                    status: "alerta",
+                                    msg: "Usuário não tem acesso à máquina"
+                                });
+                            }
+                        });
+                }
             })
-        })
+            .catch(err => {
+                return res.json({
+                    status: "erro3",
+                    msg: err
+                });
+            });
     }
-})
+});
 
 module.exports = router;

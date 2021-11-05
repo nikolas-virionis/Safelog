a6139477155e51b44e231c9c417580dda231479a
nikolas-virionis <nikolas.virionis@bandtec.com.br>
2021-11-04 22:24:15 -0300

    logica front end de dados de um chamado específico

--- website/public/assets/js/chamado.js
+++ website/public/assets/js/chamado.js
@@ -1,96 +1,66 @@
 const urlParams = new URLSearchParams(window.location.search);
 const idChamado = urlParams.get("idChamado");
 
-axios.post("/chamado/dados", { idChamado })
-.then(({data: {status, msg}}) => {
-
+axios.post("/chamado/dados", {idChamado}).then(({data: {status, msg}}) => {
     // limpando conteúdo atual do conteiner
-    const container = document.querySelector(".container-site");
-    container.innerHTML = "";
+    const container = document.querySelectorAll(".container-site")[1];
 
     if (status === "ok") {
         // renderizando chamado
-        renderChamado(msg, container);
-        console.log(msg)
-        if(msg.solucao) {
+        renderChamado(msg);
+        console.log(msg);
+        if (msg.solucao) {
             // renderizando solução
-            renderSolucao(msg.solucao, container);
-        } else {
-            // 
+            renderSolucao(msg.solucao);
         }
     } else {
-        console.warn(msg)
+        console.warn(msg);
         renderChamadoNaoEncontrado(container);
     }
-})
+});
 
 // renderiza chamado
-const renderChamado = (msg, container) => {
+const renderChamado = msg => {
     // criando box chamado
-    const divBoxChamado = document.createElement("div");
-    const divStatus = document.createElement("div");
-    const divTitulo = document.createElement("div");
-    const divPrioridade = document.createElement("div");
-    const divDescricao = document.createElement("div");
-    const divFooter = document.createElement("div");
-    const divResponsavelNome = document.createElement("div");
-    const divResponsavelEmail = document.createElement("div");
-
-    const spanPrioridade = document.createElement("span");
-    const spanDataAbertura = document.createElement("span");
+    const divStatus = document.querySelector(".status-chamado");
+    const divTitulo = document.querySelector(".titulo");
+    const divPrioridade = document.querySelector(".prioridade");
+    const divDescricao = document.querySelector(".descricao");
+    const divResponsavelNome = document.querySelector(".responsavel-nome");
+    const divResponsavelEmail = document.querySelector(".responsavel-email");
+    const divDataAbertura = document.querySelector(".data-abertura");
 
     // adicionando corpo das divs
     divStatus.innerHTML = msg.status_chamado;
     divTitulo.innerHTML = msg.titulo;
-    divPrioridade.innerHTML = "Prioridade: ";
+    divPrioridade.innerHTML = `Prioridade: <span>${msg.prioridade}</span>`;
     divDescricao.innerHTML = msg.descricao;
-    divFooter.innerHTML = "Responsável: ";
-    divResponsavelNome.innerHTML = msg.nome;
-    divResponsavelEmail.innerHTML = msg.email;
-
-    spanPrioridade.innerHTML = msg.prioridade;
-
     const data = new Date(msg.data_abertura).toLocaleString("pt-BR");
-    spanDataAbertura.innerHTML = data;
-
-    // adicionando classe às divs/spans
-    divBoxChamado.classList.add("box-chamado");
-    divStatus.classList.add("status-chamado");
-    divStatus.classList.add(msg.status_chamado);
-    divTitulo.classList.add("titulo");
-    divPrioridade.classList.add("prioridade");
-    divDescricao.classList.add("descricao");
-    divFooter.classList.add("chamado-footer");
-    divResponsavelNome.classList.add("responsavel-nome");
-    divResponsavelEmail.classList.add("responsavel-email");
-    spanDataAbertura.classList.add("data-abertura");
-
-    // aninhando divs
-    divPrioridade.appendChild(spanPrioridade);
-    divFooter.appendChild(divResponsavelNome);
-    divFooter.appendChild(divResponsavelEmail);
-    divFooter.appendChild(spanDataAbertura);
-
-    // adicionando tudo à box
-    divBoxChamado.appendChild(divStatus);
-    divBoxChamado.appendChild(divTitulo);
-    divBoxChamado.appendChild(divPrioridade);
-    divBoxChamado.appendChild(divDescricao);
-    divBoxChamado.appendChild(divFooter);
-
-    // adicionando box ao Container
-    container.appendChild(divBoxChamado);
-}
+    divDataAbertura.innerHTML = data;
+    if (msg.automatico == "n") {
+        divResponsavelNome.innerHTML = msg.nome;
+        divResponsavelEmail.innerHTML = msg.email;
+    } else {
+        divResponsavelNome.innerHTML = "Monitoramento automático";
+        divResponsavelEmail.innerHTML = "";
+    }
+    divStatus.classList = `status-chamado ${msg.status_chamado}`;
+};
 
 // renderiza solução
-const renderSolucao = (solucao, container) => {
-    // criando divs 
-    const divBox = document.createElement("div");
-    const divHeader = document.createElement("div");
-    const divTitulo = document.createElement("div");
-    const divEficacia = document.createElement("div");
-    const divDescricao = document.createElement("div");
-    const divData = document.createElement("div");
+const renderSolucao = solucao => {
+    // criando divs
+    document.querySelector(".card.solucao").style.display = "block";
+    const divTitulo = document.querySelector(".solucao-titulo");
+    const divEficacia = document.querySelector(".solucao-eficacia");
+    const divDescricao = document.querySelector(".solucao-descricao");
+    const divData = document.querySelector(".solucao-data");
+    const divResponsavelNome = document.querySelector(
+        ".solucao-responsavel-nome"
+    );
+    const divResponsavelEmail = document.querySelector(
+        ".solucao-responsavel-email"
+    );
 
     // adicionando corpo das divs
     divTitulo.innerHTML = solucao.titulo;
@@ -98,32 +68,18 @@ const renderSolucao = (solucao, container) => {
     divDescricao.innerHTML = solucao.descricao;
     const data = new Date(solucao.data_solucao);
     divData.innerHTML = data.toLocaleString("pt-BR");
+    divResponsavelNome.innerHTML = solucao.nome;
+    divResponsavelEmail.innerHTML = solucao.email;
 
     // adicionando classes
-    divBox.classList.add("box-solucao");
-    divHeader.classList.add("solucao-header");
-    divTitulo.classList.add("solucao-titulo");
-    divEficacia.classList.add("solucao-eficacia");
-    divEficacia.classList.add(`eficacia-${solucao.eficacia}`);
-    divDescricao.classList.add("solucao-descricao");
-    divData.classList.add("solucao-data");
+    divEficacia.classList = `solucao-eficacia eficacia-${solucao.eficacia}`;
+};
 
-    // aninhando divs
-    divHeader.appendChild(divTitulo);
-    divHeader.innerHTML += "Eficácia: ";
-    divHeader.appendChild(divEficacia);
-    divBox.appendChild(divHeader);
-    divBox.appendChild(divDescricao);
-    divBox.appendChild(divData);
-
-    // adicionando solucao ao container 
-    container.appendChild(divBox);
-}
-
-const renderChamadoNaoEncontrado = (container) => {
-    // criando elemento 
+const renderChamadoNaoEncontrado = container => {
+    // criando elemento
     const warnBox = document.createElement("div");
-    warnBox.innerHTML = "Chamado não encontrado! Você será redirecionado para a lista de chamados";
+    warnBox.innerHTML =
+        "Chamado não encontrado! Você será redirecionado para a lista de chamados";
 
     warnBox.classList.add("warn-box");
 
@@ -132,4 +88,4 @@ const renderChamadoNaoEncontrado = (container) => {
     setTimeout(() => {
         document.location.href = "/chamados";
     }, 2000);
-}
+};

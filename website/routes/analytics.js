955207b23327bdbd23c67215e9a0fe72dfe94770
nikolas-virionis <nikolas.virionis@bandtec.com.br>
2021-11-06 10:31:48 -0300

    logica back end de verificar trend line ultimo conjunto de dados => orientação e comportamento

--- website/routes/analytics.js
+++ website/routes/analytics.js
@@ -2,7 +2,8 @@
 let express = require("express");
 let router = express.Router();
 let sequelize = require("../models").sequelize;
-const {getTrendDeg} = require("../util/analytics/trendLine");
+const {getTrendDeg, getTrendBehavior} = require("../util/analytics/trendLine");
+const {getMedicoes} = require("../util/analytics/dados");
 
 router.post("/trend", async (req, res) => {
     let {idCategoriaMedicao} = req.body;
@@ -12,14 +13,14 @@ router.post("/trend", async (req, res) => {
             msg: "Body não fornecido na requisição"
         });
     }
-    const sqlMedicoes = `SELECT valor FROM medicao WHERE fk_categoria_medicao = ${idCategoriaMedicao} ORDER BY data_medicao DESC LIMIT 100`;
-    let medicoes = await sequelize
-        .query(sqlMedicoes, {type: sequelize.QueryTypes.SELECT})
-        .map(el => Number(el.valor));
-    medicoes = [...medicoes].reverse();
+
+    let medicoes = await getMedicoes(idCategoriaMedicao);
 
     let deg = getTrendDeg(medicoes);
-    res.json({deg});
+
+    let {orientacao, comportamento} = getTrendBehavior(deg);
+
+    res.json({stauts: "ok", msg: {orientacao, comportamento}});
 });
 
 module.exports = router;
--- website/util/analytics/trendLine.js
+++ website/util/analytics/trendLine.js
@@ -31,4 +31,29 @@ const getTrendDeg = medicoes => {
     return deg;
 };
 
-module.exports = {getTrendDeg};
+const getTrendBehavior = deg => {
+    let orientacao, comportamento;
+    if (deg >= -1 && deg <= 1) {
+        orientacao = "horizontal";
+        comportamento = "";
+    } else {
+        if (deg >= 0) {
+            orientacao = "crescimento";
+        } else {
+            orientacao = "atenuamento"; // "atenuamento"
+        }
+
+        if (Math.abs(deg) < 10) {
+            comportamento = "leve";
+        } else if (Math.abs(deg) < 25) {
+            comportamento = "moderado";
+        } else if (Math.abs(deg) < 40) {
+            comportamento = "considerável";
+        } else {
+            comportamento = "drástico";
+        }
+    }
+    return {orientacao, comportamento};
+};
+
+module.exports = {getTrendDeg, getTrendBehavior};

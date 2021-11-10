bbbfbf34ca5c4102322bc91256fa7c024a3dabac
nikolas-virionis <nikolas.virionis@bandtec.com.br>
2021-11-10 19:04:07 -0300

    Inicio da formação de notificações, reutilização dos emails que convinham e boas vindas

--- website/util/notificacao/notificacao.js
+++ website/util/notificacao/notificacao.js
@@ -1,5 +1,27 @@
-const msg = () => {
+const {msgEmail} = require("../email/msg");
+const tiposDefault = [
+    "alerta",
+    "redefinir",
+    "acesso",
+    "convite de acesso",
+    "redefinir responsavel",
+    "atribuir responsavel",
+    "convidar responsavel",
+    "notificacao remocao acesso",
+    "convite responsavel",
+    "notificacao edicao maquina",
+    "notificacao acesso"
+];
+const msg = (tipo, nome, rest, email) => {
     // criacao da notificacao
+    if (tiposDefault.includes(tipo.toLowerCase()))
+        return msgEmail(tipo, nome, rest, email);
+    if (tipo.toLowerCase() == "boas vindas")
+        return [
+            `<p>Prezado(a) ${nome},</p>
+        <p>Nós da SafeLog agradecemos fortemente sua presença aqui, e vamos construir um futuro muito produtivo juntos, isso é apenas o começo</p>`,
+            `Bem vindo(a)`
+        ];
 };
 
 module.exports = {msg};

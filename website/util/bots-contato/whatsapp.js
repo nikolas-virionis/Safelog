6ac60cdfa93d92b3cb980b7a95b82bf08053f7ac
Lucas Mesquita <lucas.msouza@bandtec.com.br>
2021-11-11 20:15:09 -0300

    função de envio de msg (whatsapp)

--- website/util/bots-contato/whatsapp.js
+++ website/util/bots-contato/whatsapp.js
@@ -1,16 +1,19 @@
 // Download the helper library from https://www.twilio.com/docs/node/install
 // Find your Account SID and Auth Token at twilio.com/console
 // and set the environment variables. See http://twil.io/secure
+require("dotenv").config();
+
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
+
 const client = require("twilio")(accountSid, authToken);
 
-function mandarWhatsapp() {
+function mandarWhatsapp(to, body) {
   client.messages
     .create({
-      from: `whatsapp:${"remetente"}`,
-      body: "Hello World!",
-      to: `whatsapp:${"destinatario"}`,
+      from: `whatsapp:+14155238886`,
+      to: `whatsapp:+55${to}`,
+      body
     })
     .then((message) => console.log(message, message.sid));
 }

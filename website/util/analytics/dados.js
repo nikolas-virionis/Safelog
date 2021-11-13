4e05979c5bcec410d4d3b5052c2ee6029c24f0c5
nikolas-virionis <nikolas.virionis@bandtec.com.br>
2021-11-13 11:17:04 -0300

    refactor calcular trend line(diferentes tempos)

--- website/util/analytics/dados.js
+++ website/util/analytics/dados.js
@@ -1,19 +1,39 @@
 let sequelize = require("../../models").sequelize;
 
-const getMedicoes = async idCategoriaMedicao => {
-    // const sqlMedicoes = `SELECT valor FROM medicao WHERE fk_categoria_medicao = ${idCategoriaMedicao} AND data_medicao BETWEEN DATE_SUB(NOW(),INTERVAL 1 WEEK) AND NOW() ORDER BY data_medicao DESC`;
+const getMedicoesTrend = async ({
+    idCategoriaMedicao,
+    type = "week",
+    qtd = 1
+} = {}) => {
+    if (!idCategoriaMedicao)
+        throw "É necessário a identificação da métrica para continuar";
 
-    const sqlMedicoes = `SELECT valor FROM medicao WHERE fk_categoria_medicao = ${idCategoriaMedicao} ORDER BY data_medicao DESC LIMIT 100`;
+    let sqlMedicoes;
 
-    // const sqlMedicoes = `SELECT valor FROM medicao WHERE fk_categoria_medicao = ${idCategoriaMedicao} ORDER BY data_medicao DESC LIMIT 200`;
+    if (type == "day") {
+        sqlMedicoes = `SELECT valor FROM medicao WHERE fk_categoria_medicao = ${idCategoriaMedicao} AND data_medicao BETWEEN DATE_SUB(NOW(),INTERVAL ${qtd} DAY) AND NOW() ORDER BY data_medicao DESC`;
+    } else if (type == "week") {
+        sqlMedicoes = `SELECT valor FROM medicao WHERE fk_categoria_medicao = ${idCategoriaMedicao} AND data_medicao BETWEEN DATE_SUB(NOW(),INTERVAL ${qtd} WEEK) AND NOW() ORDER BY data_medicao DESC`;
+    } else if (type == "month") {
+        sqlMedicoes = `SELECT valor FROM medicao WHERE fk_categoria_medicao = ${idCategoriaMedicao} AND data_medicao BETWEEN DATE_SUB(NOW(),INTERVAL ${qtd} MONTH) AND NOW() ORDER BY data_medicao DESC`;
+    } else if (type == "qtd") {
+        sqlMedicoes = `SELECT valor FROM medicao WHERE fk_categoria_medicao = ${idCategoriaMedicao} ORDER BY data_medicao DESC LIMIT ${qtd}`;
+    } else {
+        throw "erro na definição do tipo de metrica de data";
+    }
+    // let sqlMedicoes = `SELECT valor FROM medicao WHERE fk_categoria_medicao = ${idCategoriaMedicao} AND data_medicao BETWEEN DATE_SUB(NOW(),INTERVAL 1 WEEK) AND NOW() ORDER BY data_medicao DESC`;
 
-    // const sqlMedicoes = `SELECT valor FROM medicao WHERE fk_categoria_medicao = ${idCategoriaMedicao} ORDER BY data_medicao DESC LIMIT 50`;
+    // let sqlMedicoes = `SELECT valor FROM medicao WHERE fk_categoria_medicao = ${idCategoriaMedicao} ORDER BY data_medicao DESC LIMIT 100`;
 
-    // const sqlMedicoes = `SELECT valor FROM medicao WHERE fk_categoria_medicao = ${idCategoriaMedicao} AND data_medicao BETWEEN DATE_SUB(NOW(),INTERVAL 1 DAY) AND NOW() ORDER BY data_medicao DESC`;
+    // let sqlMedicoes = `SELECT valor FROM medicao WHERE fk_categoria_medicao = ${idCategoriaMedicao} ORDER BY data_medicao DESC LIMIT 200`;
 
-    // const sqlMedicoes = `SELECT valor FROM medicao WHERE fk_categoria_medicao = ${idCategoriaMedicao} AND data_medicao BETWEEN DATE_SUB(NOW(),INTERVAL 1 MONTH) AND NOW() ORDER BY data_medicao DESC`;
+    // let sqlMedicoes = `SELECT valor FROM medicao WHERE fk_categoria_medicao = ${idCategoriaMedicao} ORDER BY data_medicao DESC LIMIT 50`;
 
-    // const sqlMedicoes = `SELECT valor FROM medicao WHERE fk_categoria_medicao = ${idCategoriaMedicao} AND data_medicao BETWEEN DATE_SUB(NOW(),INTERVAL 2 WEEK) AND NOW() ORDER BY data_medicao DESC`;
+    // let sqlMedicoes = `SELECT valor FROM medicao WHERE fk_categoria_medicao = ${idCategoriaMedicao} AND data_medicao BETWEEN DATE_SUB(NOW(),INTERVAL 1 DAY) AND NOW() ORDER BY data_medicao DESC`;
+
+    // let sqlMedicoes = `SELECT valor FROM medicao WHERE fk_categoria_medicao = ${idCategoriaMedicao} AND data_medicao BETWEEN DATE_SUB(NOW(),INTERVAL 1 MONTH) AND NOW() ORDER BY data_medicao DESC`;
+
+    // let sqlMedicoes = `SELECT valor FROM medicao WHERE fk_categoria_medicao = ${idCategoriaMedicao} AND data_medicao BETWEEN DATE_SUB(NOW(),INTERVAL 2 WEEK) AND NOW() ORDER BY data_medicao DESC`;
     return await sequelize
         .query(sqlMedicoes, {type: sequelize.QueryTypes.SELECT})
         .then(medicoes => {
@@ -24,4 +44,5 @@ const getMedicoes = async idCategoriaMedicao => {
         });
 };
 
-module.exports = {getMedicoes};
+
+module.exports = {getMedicoesTrend, getStatsChamado};

f16fd0a6e8ef4243c7009550174fe874c9232b7b
nikolas-virionis <nikolas.virionis@bandtec.com.br>
2021-11-13 14:48:29 -0300

    refactor logica de lista de dependentes de analista

--- website/util/acesso-maquina/maquinasDependentes.js
+++ website/util/acesso-maquina/maquinasDependentes.js
@@ -1,28 +1,14 @@
 let sequelize = require("../../models").sequelize;
-const maquinasDependentes = async id => {
-    let dependentes = `SELECT pk_maquina, id_maquina, nome as maquina FROM maquina JOIN usuario_maquina ON fk_maquina = pk_maquina and fk_usuario = ${id}`;
+
+const maquinasDependentes = async (id, search) => {
+    let responsavel = `SELECT pk_maquina, id_maquina, maquina.nome AS maquina, usuario.nome AS responsavel FROM usuario JOIN usuario_maquina ON fk_usuario = id_usuario AND responsavel = 's' JOIN maquina ON fk_maquina = pk_maquina AND pk_maquina IN (SELECT pk_maquina FROM maquina JOIN usuario_maquina ON fk_maquina = pk_maquina AND fk_usuario = ${id}) ${
+        search
+            ? ` WHERE id_maquina LIKE '%${search}%' OR maquina.nome LIKE '%${search}%' OR usuario.nome LIKE '%${search}%'`
+            : ``
+    };`;
     return await sequelize
-        .query(dependentes, {type: sequelize.QueryTypes.SELECT})
-        .then(async response => {
-            let maquinas = [];
-            for (let {pk_maquina, id_maquina, maquina} of response) {
-                let responsavel = `SELECT usuario.nome FROM usuario JOIN usuario_maquina ON fk_usuario = id_usuario and responsavel = 's' and fk_maquina = ${pk_maquina};`;
-                await sequelize
-                    .query(responsavel, {
-                        type: sequelize.QueryTypes.SELECT
-                    })
-                    .then(([{nome: usuario}]) =>
-                        maquinas.push({
-                            pk_maquina,
-                            id_maquina,
-                            maquina,
-                            responsavel: usuario
-                        })
-                    )
-                    .catch(err => ({status: "erro", msg: err}));
-            }
-            return maquinas;
-        })
+        .query(responsavel, {type: sequelize.QueryTypes.SELECT})
+        .then(response => response)
         .catch(err => ({status: "erro", msg: err}));
 };
 module.exports = {maquinasDependentes};

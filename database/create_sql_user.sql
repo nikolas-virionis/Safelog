f416d6079a1b1abf8e81e986abdfa49a47fd712d
nikolas-virionis <nikolas.virionis@bandtec.com.br>
2021-11-20 11:43:38 -0300

    adequação  private ip para VM

--- database/create_sql_user.sql
+++ database/create_sql_user.sql
@@ -2,7 +2,7 @@ CREATE USER 'safelog_dev' @'localhost' IDENTIFIED BY 'Safe_Log371$';
 GRANT ALL PRIVILEGES ON `safelog`.* TO `safelog_dev` @`localhost`;
 
 -- docker mysql user
-CREATE USER 'safelog_dev'@'172.17.0.1' IDENTIFIED BY 'Safe_Log371$';
-GRANT ALL PRIVILEGES ON `safelog`.* TO 'safelog_dev'@'172.17.0.1';
+CREATE USER 'safelog_dev'@'172.31.25.218' IDENTIFIED BY 'Safe_Log371$';
+GRANT ALL PRIVILEGES ON `safelog`.* TO 'safelog_dev'@'172.31.25.218';
 
 FLUSH PRIVILEGES;

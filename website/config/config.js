b9daf092dc07f12cc1445c028aa4205ea2a27bb3
nikolas-virionis <nikolas.virionis@bandtec.com.br>
2021-11-21 18:18:23 -0300

    config.js: dns da aws para db

--- website/config/config.js
+++ website/config/config.js
@@ -6,20 +6,20 @@ module.exports = {
         username: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME,
-        host: "localhost", // (localhost da ec2, onde o express estará rodando em producão)
+        host: "ec2-34-229-174-105.compute-1.amazonaws.com", // (localhost da ec2, onde o express estará rodando em producão)
         dialect: "mysql",
         xuse_env_variable: "DATABASE_URL",
         dialectOptions: {
             options: {
-                encrypt: true,
-            },
+                encrypt: true
+            }
         },
         pool: {
             max: 5,
             min: 1,
             acquire: 5000,
-            connectTimeout: 5000,
-        },
+            connectTimeout: 5000
+        }
     },
 
     // azure (SQL SERVER)
@@ -33,15 +33,15 @@ module.exports = {
         xuse_env_variable: "srvsafelog.database.windows.net",
         dialectOptions: {
             options: {
-                encrypt: true,
-            },
+                encrypt: true
+            }
         },
         pool: {
             max: 5,
             min: 1,
             acquire: 5000,
-            connectTimeout: 5000,
-        },
+            connectTimeout: 5000
+        }
     },
 
     // dev / banco local
@@ -54,15 +54,15 @@ module.exports = {
         xuse_env_variable: "DATABASE_URL",
         dialectOptions: {
             options: {
-                encrypt: true,
-            },
+                encrypt: true
+            }
         },
         pool: {
             max: 5,
             min: 1,
             acquire: 5000,
             idle: 30000,
-            connectTimeout: 5000,
-        },
-    },
+            connectTimeout: 5000
+        }
+    }
 };

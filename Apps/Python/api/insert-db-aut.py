89f1a0717fa211d694e7d112c51588830b192f24
nikolas-virionis <nikolas.virionis@bandtec.com.br>
2021-11-22 20:13:37 -0300

    refactor api python, para comportar novas conexões com BD + fallack e redundancia azure

--- Apps/Python/api/insert-db-aut.py
+++ Apps/Python/api/insert-db-aut.py
@@ -7,27 +7,27 @@ import mysql.connector as sql
 from credentials import usr, pswd
 import pyodbc
 
+print("Adquirindo preferências de usuario")
 db_connection = ""
 try:
     db_connection = sql.connect(host='172.31.25.218', database='safelog',
-                            user=usr, password=pswd)
-except: 
+                                user=usr, password=pswd)
+except Exception:
     server = 'tcp:srvsafelog.database.windows.net'
     database = 'safelog'
     username = usr
     password = pswd
-    db_connection = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER=' +
-                              server+';DATABASE='+database+';UID='+username+';PWD=' + password)
-                              
+    db_connection = pyodbc.connect('DRIVER={SQL Server};SERVER=' +
+                                   server+';DATABASE='+database+';UID='+username+';PWD=' + password)
 preferences = pd.read_sql(
-    f"SELECT tipo_medicao.tipo FROM categoria_medicao INNER JOIN tipo_medicao ON fk_tipo_medicao = id_tipo_medicao WHERE fk_maquina = (SELECT pk_maquina FROM maquina WHERE id_maquina = '{mac_addr()}')", con=db_connection)
+    f"SELECT tipo_medicao.tipo, unidade FROM categoria_medicao INNER JOIN tipo_medicao ON fk_tipo_medicao = id_tipo_medicao WHERE fk_maquina = (SELECT pk_maquina FROM maquina WHERE id_maquina = '{mac_addr()}')", con=db_connection)
 db_connection.close()
 try:
     while True:
         data_medicao = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
         medicao = 0
         dados = [data_medicao]
-        for tipo in preferences["tipo"]:
+        for index, tipo in enumerate(preferences["tipo"]):
             if tipo == "cpu_frequencia":
                 medicao = round(psutil.cpu_freq().current *
                                 100 / psutil.cpu_freq().max, 2)
@@ -43,7 +43,9 @@ try:
                 medicao = round(psutil.disk_usage('/').percent, 2)
             else:
                 continue
-            dados.append({"tipo": tipo, "medicao": medicao})
+            print(f'{tipo}: {medicao}{preferences["unidade"][index]}')
+
+            dados.append({'tipo': tipo, 'medicao': medicao})
         insert_db(dados)
         time.sleep(1)
 except KeyboardInterrupt:

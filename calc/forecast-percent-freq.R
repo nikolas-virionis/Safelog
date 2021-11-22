a337389847f534f3222af308f1c1dc3a4d62cab4
lucasmsouza24 <lucas.msouza@bandtec.com.br>
2021-11-22 14:34:02 -0300

    forecast com previsao

--- calc/forecast-percent-freq.R
+++ calc/forecast-percent-freq.R
@@ -3,17 +3,31 @@ library(ggplot2)
 # defining x and y
 x <- amostraDesktop1$cpu_porcentagem
 y <- amostraDesktop1$cpu_frequencia
-dados <- data.frame(percent=x, freq=y)
+dados <- data.frame(cpu_porcentagem=x, cpu_frequencia=y)
 
 # view alfa and beta
-lm(x~y)
+lm(data=dados, formula=dados$cpu_frequencia~dados$cpu_porcentagem)
 
 # linear regression
-ggplot(dados, aes(x=x, y=y)) + geom_point() + geom_smooth(method=lm, se = FALSE)
+ggplot(dados, aes(x=dados$cpu_porcentagem, y=dados$cpu_frequencia)) + geom_point() + geom_smooth(method=lm, se = FALSE)
 
 # correlation
 cor(x, y)
 
+previsao <- function(x) {
+    return(-95.432 + 1.614 * x)
+}
+
+
+
 # predict
 modelo <- lm(data=dados, formula=x~y)
 predict(modelo)
+
+
+for(i in 1:578) {
+    if(data2$x[i] > 70) {
+        x <- c(x, data2$x[i])
+        y <- c(y, data2$y[i])
+    }
+}

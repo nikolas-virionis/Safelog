3cdd925aa30de5485611aafd914e1d987090f819
lucasmsouza24 <lucas.msouza@bandtec.com.br>
2021-11-22 20:29:26 -0300

    tratamento de outliers

--- calc/forecast-percent-freq.R
+++ calc/forecast-percent-freq.R
@@ -6,7 +6,7 @@ y <- amostraDesktop1$cpu_frequencia
 dados <- data.frame(cpu_porcentagem=x, cpu_frequencia=y)
 
 # view alfa and beta
-lm(data=dados, formula=dados$cpu_frequencia~dados$cpu_porcentagem)
+model1 <- lm(data=dados, formula=dados$cpu_frequencia~dados$cpu_porcentagem)
 
 # linear regression
 ggplot(dados, aes(x=dados$cpu_porcentagem, y=dados$cpu_frequencia)) + geom_point() + geom_smooth(method=lm, se = FALSE)
@@ -18,16 +18,59 @@ previsao <- function(x) {
     return(-95.432 + 1.614 * x)
 }
 
-
-
 # predict
-modelo <- lm(data=dados, formula=x~y)
+model2 <- lm(data=dados, formula=x~y)
 predict(modelo)
 
-
+###################################################################################################################
+# removendo x < 70
 for(i in 1:578) {
     if(data2$x[i] > 70) {
         x <- c(x, data2$x[i])
         y <- c(y, data2$y[i])
     }
 }
+
+ggplot(dados, aes(x=dados$cpu_porcentagem, y=dados$cpu_frequencia)) + geom_point() + geom_smooth(method=lm, se = FALSE)
+
+##################################################################################################################
+# tratamento de dados (removendo outliers do y)
+dados2 <- data.frame(cpu_porcentagem=dados$cpu_porcentagem, cpu_frequencia=dados$cpu_frequencia)
+
+x2 <- c()
+y2 <- c()
+
+for(i in 1:578) {
+
+    if (dados$cpu_frequencia[i] < 65 & dados$cpu_frequencia[i] > 5) {
+        x2 <- c(x2, dados$cpu_porcentagem[i])
+        y2 <- c(y2, dados$cpu_frequencia[i])
+    }
+}
+
+dados2 <- data.frame(cpu_porcentagem=x2, cpu_frequencia=y2)
+
+ggplot(dados2, aes(x=dados2$cpu_porcentagem, y=dados2$cpu_frequencia)) + geom_point() + geom_smooth(method=lm, se = FALSE)
+
+cor(x2, y2))
+
+#################################################################################################################
+# tratamento de dados (removendo outliers do x)
+dados3 <- data.frame(cpu_porcentagem=dados2$cpu_porcentagem, cpu_frequencia=dados2$cpu_frequencia)
+
+x3 <- c()
+y3 <- c()
+
+for(i in 1:length(dados2$cpu_porcentagem)) {
+
+    if (dados2$cpu_porcentagem[i] < 80 & dados2$cpu_porcentagem[i] > 68) {
+        x3 <- c(x3, dados$cpu_porcentagem[i])
+        y3 <- c(y3, dados$cpu_frequencia[i])
+    }
+}
+
+dados3 <- data.frame(cpu_porcentagem=x2, cpu_frequencia=y2)
+
+ggplot(dados3, aes(x=dados3$cpu_porcentagem, y=dados3$cpu_frequencia)) + geom_point() + geom_smooth(method=lm, se = FALSE)
+
+cor(x3, y3))

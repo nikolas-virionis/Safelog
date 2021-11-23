c815ffc19b8b01bdaa44165f2ee451f170caf19f
nikolas-virionis <nikolas.virionis@bandtec.com.br>
2021-11-23 08:21:29 -0300

    logica de fallback para envio de email

--- website/util/email/email.js
+++ website/util/email/email.js
@@ -2,20 +2,33 @@ const {msgEmail} = require("./msg");
 const mandarEmail = async (tipo, nome, destinatario, rest) => {
     let nodemailer = require("nodemailer");
     require("dotenv").config();
-    let remetente = process.env.EMAIL;
+    let remetente;
     let senha = process.env.PASSWORD;
-    let transporter = await nodemailer.createTransport({
-        host: "smtp-mail.outlook.com", // hostname
-        secureConnection: false, // TLS requires secureConnection to be false
-        port: 587, // port for secure SMTP
-        tls: {
-            ciphers: "SSLv3"
-        },
-        auth: {
-            user: remetente,
-            pass: senha
-        }
-    });
+    let transporter;
+    try {
+        remetente = process.env.EMAIL;
+        transporter = await nodemailer.createTransport({
+            service: "gmail", // hostname
+            auth: {
+                user: remetente,
+                pass: senha
+            }
+        });
+    } catch (e) {
+        remetente = process.env.EMAIL_FALLBACK;
+        transporter = await nodemailer.createTransport({
+            host: "smtp-mail.outlook.com", // hostname
+            secureConnection: false, // TLS requires secureConnection to be false
+            port: 587, // port for secure SMTP
+            tls: {
+                ciphers: "SSLv3"
+            },
+            auth: {
+                user: remetente,
+                pass: senha
+            }
+        });
+    }
     const mailOptions = {
         from: remetente, // sender address
         to: destinatario, // list of receivers

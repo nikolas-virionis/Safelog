10d78ddea368103d5af8d48c3550aa8d0fb0b677
nikolas-virionis <nikolas.virionis@bandtec.com.br>
2021-11-22 22:42:06 -0300

    tratamento de resposta nos axios de chamado

--- website/public/assets/js/chamado.js
+++ website/public/assets/js/chamado.js
@@ -55,21 +55,29 @@ const renderChamado = msg => {
         statusChamado.classList = "status emergencia";
     }
 
-    if(msg.status_chamado == "aberto"){
-        if(msg.fk_usuario == userInfo.id){
-            document.querySelector("#btnEditar").classList.remove("display-none");
-            document.querySelector("#btnEditar").addEventListener("click", () => {
-                cardAlterarChamado("editar");
-            });
-        }else{
-            document.querySelector("#btnResponder").classList.remove("display-none");
-            document.querySelector("#btnResponder").addEventListener("click", () => {
-                cardAlterarChamado("responder");
-            });
+    if (msg.status_chamado == "aberto") {
+        if (msg.fk_usuario == userInfo.id) {
+            document
+                .querySelector("#btnEditar")
+                .classList.remove("display-none");
+            document
+                .querySelector("#btnEditar")
+                .addEventListener("click", () => {
+                    cardAlterarChamado("editar");
+                });
+        } else {
+            document
+                .querySelector("#btnResponder")
+                .classList.remove("display-none");
+            document
+                .querySelector("#btnResponder")
+                .addEventListener("click", () => {
+                    cardAlterarChamado("responder");
+                });
         }
     }
 
-    console.log(msg)
+    console.log(msg);
     nomeMaquina.innerHTML = msg.maquina;
     medicao.innerHTML = getTipo(msg.tipo); //Alterar para o nome do componente
     divDescricao.innerHTML = msg.descricao;
@@ -90,24 +98,31 @@ const renderChamado = msg => {
     document.querySelector("#inpTituloEditar").value = msg.titulo;
     document.querySelector("#txtDescricaoEdicao").innerHTML = msg.descricao;
     document.querySelector("#prioridadeEdicao").value = msg.prioridade;
-
 };
 
 const cardAlterarChamado = tipo => {
-    switch(tipo){
+    switch (tipo) {
         case "editar":
-            document.querySelector("#editarChamado").classList.remove("display-none");
-            document.querySelector("#responder-btn").classList.add("display-none");
-            
+            document
+                .querySelector("#editarChamado")
+                .classList.remove("display-none");
+            document
+                .querySelector("#responder-btn")
+                .classList.add("display-none");
+
             break;
         case "responder":
-            document.querySelector("#responderChamado").classList.remove("display-none");
-            document.querySelector("#responder-btn").classList.add("display-none");
+            document
+                .querySelector("#responderChamado")
+                .classList.remove("display-none");
+            document
+                .querySelector("#responder-btn")
+                .classList.add("display-none");
             break;
         default:
             console.log("Parâmetro incorreto");
     }
-}
+};
 
 // renderiza solução
 const renderSolucao = solucao => {
@@ -153,35 +168,55 @@ const renderChamadoNaoEncontrado = container => {
     }, 2000);
 };
 
-
 //Edição de chamado
-document.querySelector("#btnEditarChamado").addEventListener("click", ()=>{
-    const tituloEdicao = document.querySelector("#inpTituloEditar").value
-    const prioridadeEdicao = document.querySelector("#prioridadeEdicao").value
-    const descricaoEdicao = document.querySelector("#txtDescricaoEdicao").value
-    
-    axios.post("chamado/atualizar", {
-        idChamado: idChamado,
-        titulo: tituloEdicao,
-        descricao: descricaoEdicao,
-        prioridade: prioridadeEdicao,
-        eficaciaSolucoes: 0
-    }).then(() => {
-        window.location.reload();
-    });
+document.querySelector("#btnEditarChamado").addEventListener("click", () => {
+    const tituloEdicao = document.querySelector("#inpTituloEditar").value;
+    const prioridadeEdicao = document.querySelector("#prioridadeEdicao").value;
+    const descricaoEdicao = document.querySelector("#txtDescricaoEdicao").value;
+
+    axios
+        .post("chamado/atualizar", {
+            idChamado: idChamado,
+            titulo: tituloEdicao,
+            descricao: descricaoEdicao,
+            prioridade: prioridadeEdicao,
+            eficaciaSolucoes: 0
+        })
+        .then(({data: {status, msg}}) => {
+            if (status == "ok") {
+                mostrarAlerta(msg, "success");
+                setTimeout(() => window.location.reload(), 3000);
+            } else if (status == "alerta") {
+                mostrarAlerta(msg, "warning");
+            } else {
+                console.error(msg);
+            }
+        })
+        .catch(err => console.error(err));
 });
 
 //Responder chamado
-document.querySelector("#btnResponderChamado").addEventListener("click", ()=>{
-    const inpTituloResposta = document.querySelector("#inpTituloResposta").value
-    const txtReaposta = document.querySelector("#txtReaposta").value
+document.querySelector("#btnResponderChamado").addEventListener("click", () => {
+    const inpTituloResposta =
+        document.querySelector("#inpTituloResposta").value;
+    const txtReaposta = document.querySelector("#txtReaposta").value;
 
-    axios.post("chamado/fechar", {
-        idChamado: idChamado,
-        titulo: inpTituloResposta,
-        desc: txtReaposta,
-        idUsuario: userInfo.id
-    }).then(()=>{
-        window.location.reload();
-    });
+    axios
+        .post("chamado/fechar", {
+            idChamado: idChamado,
+            titulo: inpTituloResposta,
+            desc: txtReaposta,
+            idUsuario: userInfo.id
+        })
+        .then(({data: {status, msg}}) => {
+            if (status == "ok") {
+                mostrarAlerta(msg, "success");
+                setTimeout(() => window.location.reload(), 3000);
+            } else if (status == "alerta") {
+                mostrarAlerta(msg, "warning");
+            } else {
+                console.error(msg);
+            }
+        })
+        .catch(err => console.error(err));
 });

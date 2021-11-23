454dfb25622cbd1a6263c5e7fb0699691209df42
nikolas-virionis <nikolas.virionis@bandtec.com.br>
2021-11-23 07:46:27 -0300

    mudança de gmail para outlook, para aws

--- website/util/email/email.js
+++ website/util/email/email.js
@@ -1,25 +1,30 @@
-const { msgEmail } = require("./msg");
+const {msgEmail} = require("./msg");
 const mandarEmail = async (tipo, nome, destinatario, rest) => {
     let nodemailer = require("nodemailer");
     require("dotenv").config();
     let remetente = process.env.EMAIL;
     let senha = process.env.PASSWORD;
     let transporter = await nodemailer.createTransport({
-        host: 'smtp.gmail.com',
-        port: 465,
-        secure: true, 
+        host: "smtp-mail.outlook.com", // hostname
+        secureConnection: false, // TLS requires secureConnection to be false
+        port: 587, // port for secure SMTP
+        tls: {
+            ciphers: "SSLv3"
+        },
         auth: {
             user: remetente,
-            pass: senha,
-        },
+            pass: senha
+        }
     });
     const mailOptions = {
         from: remetente, // sender address
         to: destinatario, // list of receivers
         subject: msgEmail(tipo, nome, rest, destinatario)[1], // Subject line
-        html: msgEmail(tipo, nome, rest, destinatario)[0], // plain text body
+        html: msgEmail(tipo, nome, rest, destinatario)[0] // plain text body
     };
-    transporter.sendMail(mailOptions, (err, info) => console.log(err || `ALERTA - email enviado para ${info.accepted[0]}`));
+    transporter.sendMail(mailOptions, (err, info) =>
+        console.log(err || `ALERTA - email enviado para ${info.accepted[0]}`)
+    );
 };
 
-module.exports = { mandarEmail };
+module.exports = {mandarEmail};

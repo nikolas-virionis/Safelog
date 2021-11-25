88fa1569c1a794eae47754566cec33641c0bb24d
nikolas-virionis <nikolas.virionis@bandtec.com.br>
2021-11-25 07:49:55 -0300

    inicio logica de front end de envio de relatorio por email

--- website/public/assets/js/relatorio.js
+++ website/public/assets/js/relatorio.js
@@ -3,37 +3,33 @@ const cargoPessoa = JSON.parse(sessionStorage.getItem("usuario")).cargo;
 const maquinas = document.querySelector("#maquinas");
 
 const data = {
-    labels: [
-        'Críticas',
-        'Risco',
-        'Normal'
-    ],
-    datasets: [{
-        label: 'My First Dataset',
-        data: [1,0,0],
-        backgroundColor: [
-            '#ff0000',
-            '#ff8000',
-            '#0071ce'
-        ],
-        hoverOffset: 4
-    }]
+    labels: ["Críticas", "Risco", "Normal"],
+    datasets: [
+        {
+            label: "My First Dataset",
+            data: [1, 0, 0],
+            backgroundColor: ["#ff0000", "#ff8000", "#0071ce"],
+            hoverOffset: 4
+        }
+    ]
 };
 
 const config = {
-    type: 'doughnut',
-    data: data,
+    type: "doughnut",
+    data: data
 };
 
 const graficoMetricas = new Chart(
-    document.getElementById('chartDoughnut'),
+    document.getElementById("chartDoughnut"),
     config
 );
 
-console.log(cargoPessoa)
-axios.post(`/maquina/lista-dependentes/${cargoPessoa}`, {
-    id
-    }).then(({data: {status, msg}}) => {
+console.log(cargoPessoa);
+axios
+    .post(`/maquina/lista-dependentes/${cargoPessoa}`, {
+        id
+    })
+    .then(({data: {status, msg}}) => {
         if (status == "ok") {
             msg.forEach(({pk_maquina, maquina}) => {
                 option = document.createElement("option");
@@ -44,40 +40,41 @@ axios.post(`/maquina/lista-dependentes/${cargoPessoa}`, {
         } else {
             console.error(msg);
         }
-        attMetricas()
-        mostrarInfoMedicoes()
-        mostrarInfoChamado()
-        mostrarInfoTrendline()
-});
-    
-    
-    const getTipo = tipo => {
-        let metrica = tipo.split("_");
-        metrica = `${metrica[0].toUpperCase()} - ${
-            metrica[1].charAt(0).toUpperCase() + metrica[1].slice(1)
-        }`;
-        return metrica;
-    };
-    
+        attMetricas();
+        mostrarInfoMedicoes();
+        mostrarInfoChamado();
+        mostrarInfoTrendline();
+    });
+
+const getTipo = tipo => {
+    let metrica = tipo.split("_");
+    metrica = `${metrica[0].toUpperCase()} - ${
+        metrica[1].charAt(0).toUpperCase() + metrica[1].slice(1)
+    }`;
+    return metrica;
+};
+
 maquinas.addEventListener("change", () => {
-    attMetricas()
-    mostrarInfoMedicoes()
-    mostrarInfoChamado()
-    mostrarInfoTrendline()
-})
+    attMetricas();
+    mostrarInfoMedicoes();
+    mostrarInfoChamado();
+    mostrarInfoTrendline();
+});
 
 metricas.addEventListener("change", () => {
-    mostrarInfoMedicoes()
-    mostrarInfoChamado()
-    mostrarInfoTrendline()
+    mostrarInfoMedicoes();
+    mostrarInfoChamado();
+    mostrarInfoTrendline();
 });
 
 const attMetricas = () => {
     metricas.innerHTML = "";
     if (maquinas.value) {
-        axios.post("/maquina/lista-componentes", {
-            id: Number(maquinas.value)
-        }).then(({data: {status, msg}}) => {
+        axios
+            .post("/maquina/lista-componentes", {
+                id: Number(maquinas.value)
+            })
+            .then(({data: {status, msg}}) => {
                 if (status === "ok") {
                     option = document.createElement("option");
                     option.innerText = "Todas";
@@ -86,7 +83,10 @@ const attMetricas = () => {
                     msg.forEach(({id_categoria_medicao, tipo}) => {
                         option = document.createElement("option");
                         option.value = id_categoria_medicao;
-                        option.setAttribute("id",`medicao${id_categoria_medicao}`)
+                        option.setAttribute(
+                            "id",
+                            `medicao${id_categoria_medicao}`
+                        );
                         option.innerText = getTipo(tipo);
                         metricas.appendChild(option);
                     });
@@ -95,121 +95,202 @@ const attMetricas = () => {
     } else {
         metricas.disabled = true;
     }
-}
+};
 
 const mostrarInfoMedicoes = () => {
+    if (metricas.value == 0) {
+        axios
+            .post("/medicao/stats", {
+                maquina: maquinas.value
+                // idCategoriaMedicao: metricas.value
+            })
+            .then(({data: {status, msg}}) => {
+                if (status == "ok") {
+                    document.querySelector("#medicoesTotais").innerHTML =
+                        msg.medicoesTotais;
+                    document.querySelector("#medicaoCritica").innerHTML =
+                        msg.medicoesCriticas;
+                    document.querySelector("#medicaoRisco").innerHTML =
+                        msg.medicoesDeRisco;
+                    document.querySelector("#medicaoNormal").innerHTML =
+                        msg.medicoesNormais;
 
-    if(metricas.value == 0){
-        axios.post("/medicao/stats", {
-            maquina: maquinas.value,
-            // idCategoriaMedicao: metricas.value  
-            
-        }).then(({data: {status, msg}}) => {
-            if(status == "ok"){
-                document.querySelector("#medicoesTotais").innerHTML = msg.medicoesTotais
-                document.querySelector("#medicaoCritica").innerHTML = msg.medicoesCriticas
-                document.querySelector("#medicaoRisco").innerHTML = msg.medicoesDeRisco
-                document.querySelector("#medicaoNormal").innerHTML = msg.medicoesNormais
+                    let percCrit = (
+                        (msg.medicoesCriticas * 100) /
+                        msg.medicoesTotais
+                    ).toFixed(2);
+                    let percRisc = (
+                        (msg.medicoesDeRisco * 100) /
+                        msg.medicoesTotais
+                    ).toFixed(2);
+                    let percNorm = (
+                        (msg.medicoesNormais * 100) /
+                        msg.medicoesTotais
+                    ).toFixed(2);
 
-                let percCrit = (msg.medicoesCriticas*100/msg.medicoesTotais).toFixed(2);
-                let percRisc = (msg.medicoesDeRisco*100/msg.medicoesTotais).toFixed(2);
-                let percNorm = (msg.medicoesNormais*100/msg.medicoesTotais).toFixed(2);
+                    graficoMetricas.data.datasets[0].data = [
+                        percCrit,
+                        percRisc,
+                        percNorm
+                    ];
+                    graficoMetricas.update();
+                } else {
+                    console.log(status);
+                }
+            });
+    } else {
+        axios
+            .post("/medicao/stats", {
+                idCategoriaMedicao: metricas.value
+            })
+            .then(({data: {status, msg}}) => {
+                if (status == "ok") {
+                    document.querySelector("#medicoesTotais").innerHTML =
+                        msg.medicoesTotais;
+                    document.querySelector("#medicaoCritica").innerHTML =
+                        msg.medicoesCriticas;
+                    document.querySelector("#medicaoRisco").innerHTML =
+                        msg.medicoesDeRisco;
+                    document.querySelector("#medicaoNormal").innerHTML =
+                        msg.medicoesNormais;
 
-                graficoMetricas.data.datasets[0].data = [percCrit, percRisc, percNorm]
-                graficoMetricas.update()
-            }else{
-                console.log(status)
-            }
-        })
-    }else{
-        axios.post("/medicao/stats", {
-            idCategoriaMedicao: metricas.value              
-        }).then(({data: {status, msg}}) => {
-            if(status == "ok"){
-                document.querySelector("#medicoesTotais").innerHTML = msg.medicoesTotais
-                document.querySelector("#medicaoCritica").innerHTML = msg.medicoesCriticas
-                document.querySelector("#medicaoRisco").innerHTML = msg.medicoesDeRisco
-                document.querySelector("#medicaoNormal").innerHTML = msg.medicoesNormais
-    
-                let percCrit = (msg.medicoesCriticas*100/msg.medicoesTotais).toFixed(2);
-                let percRisc = (msg.medicoesDeRisco*100/msg.medicoesTotais).toFixed(2);
-                let percNorm = (msg.medicoesNormais*100/msg.medicoesTotais).toFixed(2);
-    
-                graficoMetricas.data.datasets[0].data = [percCrit, percRisc, percNorm]
-                graficoMetricas.update()
-            }else{
-                console.log(status)
-            }
-        })
+                    let percCrit = (
+                        (msg.medicoesCriticas * 100) /
+                        msg.medicoesTotais
+                    ).toFixed(2);
+                    let percRisc = (
+                        (msg.medicoesDeRisco * 100) /
+                        msg.medicoesTotais
+                    ).toFixed(2);
+                    let percNorm = (
+                        (msg.medicoesNormais * 100) /
+                        msg.medicoesTotais
+                    ).toFixed(2);
+
+                    graficoMetricas.data.datasets[0].data = [
+                        percCrit,
+                        percRisc,
+                        percNorm
+                    ];
+                    graficoMetricas.update();
+                } else {
+                    console.log(status);
+                }
+            });
     }
-
-}
-
+};
 
 const mostrarInfoChamado = () => {
-    axios.post("/chamado/stats", {
-        maquina: maquinas.value
-    }).then(({data: {status, msg}}) => {
-        if(status == "ok"){
-            document.querySelector("#totalChamados").innerHTML = msg.chamadosTotais
-            document.querySelector("#chamadosAbertos").innerHTML = msg.chamadosAbertos
-            document.querySelector("#chamadosFechados").innerHTML = msg.chamadosFechados
-            
-            document.querySelector("#totalSolucoes").innerHTML = msg.solucoesTotais
-            document.querySelector("#eficaciaTotal").innerHTML = msg.solucoesEficazes
-            document.querySelector("#eficaciaParcial").innerHTML = msg.solucoesParciais
+    axios
+        .post("/chamado/stats", {
+            maquina: maquinas.value
+        })
+        .then(({data: {status, msg}}) => {
+            if (status == "ok") {
+                document.querySelector("#totalChamados").innerHTML =
+                    msg.chamadosTotais;
+                document.querySelector("#chamadosAbertos").innerHTML =
+                    msg.chamadosAbertos;
+                document.querySelector("#chamadosFechados").innerHTML =
+                    msg.chamadosFechados;
 
-        }else{
-            console.log(status)
-        }
-    })
-}
-
+                document.querySelector("#totalSolucoes").innerHTML =
+                    msg.solucoesTotais;
+                document.querySelector("#eficaciaTotal").innerHTML =
+                    msg.solucoesEficazes;
+                document.querySelector("#eficaciaParcial").innerHTML =
+                    msg.solucoesParciais;
+            } else {
+                console.log(status);
+            }
+        });
+};
 
 const mostrarInfoTrendline = () => {
     document.querySelector("#tableTrendline").innerHTML = "";
-    if(metricas.value > 0){
-        axios.post("/analytics/trend", {
-            idCategoriaMedicao: metricas.value
-        }).then(({data: {msg, status}}) => {
-            console.log(status)
-            console.log(msg)
-            let tr = document.createElement("tr");
-            let tdMetrica = document.createElement("td");
-            let tdTendencia = document.createElement("td");
-            tr.appendChild(tdMetrica);
-            tr.appendChild(tdTendencia);
+    if (metricas.value > 0) {
+        axios
+            .post("/analytics/trend", {
+                idCategoriaMedicao: metricas.value
+            })
+            .then(({data: {msg, status}}) => {
+                console.log(status);
+                console.log(msg);
+                let tr = document.createElement("tr");
+                let tdMetrica = document.createElement("td");
+                let tdTendencia = document.createElement("td");
+                tr.appendChild(tdMetrica);
+                tr.appendChild(tdTendencia);
 
-            tdMetrica.innerHTML = document.getElementById(`medicao${metricas.value}`).innerHTML;
-            tdTendencia.innerHTML = `${msg.orientacao} ${msg.comportamento}`
-            document.querySelector("#tableTrendline").appendChild(tr);
-        });
-    }else{
-        axios.post("/maquina/lista-componentes", {
-            id: Number(maquinas.value)
-        }).then(({data: {status, msg}}) => {
+                tdMetrica.innerHTML = document.getElementById(
+                    `medicao${metricas.value}`
+                ).innerHTML;
+                tdTendencia.innerHTML = `${msg.orientacao} ${msg.comportamento}`;
+                document.querySelector("#tableTrendline").appendChild(tr);
+            });
+    } else {
+        axios
+            .post("/maquina/lista-componentes", {
+                id: Number(maquinas.value)
+            })
+            .then(({data: {status, msg}}) => {
                 if (status == "ok") {
-                    
                     msg.forEach(({id_categoria_medicao}) => {
                         let tr = document.createElement("tr");
                         let tdMetrica = document.createElement("td");
                         let tdTendencia = document.createElement("td");
                         tr.appendChild(tdMetrica);
                         tr.appendChild(tdTendencia);
-            
-                        tdMetrica.innerHTML = document.getElementById(`medicao${id_categoria_medicao}`).innerHTML;
-                        
 
-                        axios.post("/analytics/trend", {
-                            idCategoriaMedicao: id_categoria_medicao
-                        }).then(({data: {msg, status}}) => {
-                            tdTendencia.innerHTML = `${msg.orientacao} ${msg.comportamento}`
-                        });
+                        tdMetrica.innerHTML = document.getElementById(
+                            `medicao${id_categoria_medicao}`
+                        ).innerHTML;
 
+                        axios
+                            .post("/analytics/trend", {
+                                idCategoriaMedicao: id_categoria_medicao
+                            })
+                            .then(({data: {msg, status}}) => {
+                                tdTendencia.innerHTML = `${msg.orientacao} ${msg.comportamento}`;
+                            });
 
-                        document.querySelector("#tableTrendline").appendChild(tr);
+                        document
+                            .querySelector("#tableTrendline")
+                            .appendChild(tr);
                     });
                 }
             });
     }
-}
+};
+const btnEmail = document.querySelector("#botao-email");
+btnEmail.addEventListener("click", async e => {
+    e.preventDefault();
+    import("./modal.js").then(({abrirModal}) =>
+        abrirModal("modal-email-relatorio")
+    );
+    let elMaquinas;
+    const btnTodas = document.querySelector("#btn-todas");
+    const btnMaquina = document.querySelector("#btn-maquina");
+    const btnCancelar = document.querySelector(".cancelar");
+    btnCancelar.addEventListener("click", () => {
+        import("./modal.js").then(({fecharModal}) =>
+            fecharModal("modal-email-relatorio")
+        );
+    });
+    btnTodas.addEventListener("click", () => {
+        elMaquinas = maquinas.children;
+        getMaquinas(elMaquinas);
+    });
+    btnMaquina.addEventListener("click", () => {
+        elMaquinas = maquinas.selectedOptions;
+        getMaquinas(elMaquinas);
+    });
+});
+
+const getMaquinas = elMaquinas => {
+    maquinasRelatorio = [...elMaquinas].map(elMaquina => ({
+        pk_maquina: Number(elMaquina.value),
+        nomeMaquina: elMaquina.text
+    }));
+    
+};

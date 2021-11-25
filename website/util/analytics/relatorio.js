be39663aa887d49a1b3f9e6232a7bc39e775660a
nikolas-virionis <nikolas.virionis@bandtec.com.br>
2021-11-25 08:46:56 -0300

    continuação logica back end de envio de email de relatorio: modificação lista de maquinas com fk_categoria

--- website/util/analytics/relatorio.js
+++ website/util/analytics/relatorio.js
@@ -2,13 +2,20 @@ let {sequelize, sequelizeAzure} = require("../../models");
 
 const relatorio = async (maquinas, cargo) => {
     let {queryDate, data} = getDates(cargo);
+    maquinas.map(async el => {
+        let sqlFks = `SELECT id_categoria_medicao FROM categoria_medicao WHERE fk_maquina = ${el.pk_maquina}`;
+        return await sequelize
+            .query(sqlFks, {type: sequelize.QueryTypes.SELECT})
+            .then(fks => ({...el, fksCategoria: fks}))
+            .catch(err => console.error(err));
+    });
 };
 
-const statsChamado = async (maquinas, queryDate, data) => {};
+const statsChamado = async (maquinas, queryDate) => {};
 
-const statsMedicao = async (maquinas, queryDate, data) => {};
+const statsMedicao = async (maquinas, queryDate) => {};
 
-const statsTrend = async (maquinas, queryDate, data) => {};
+const statsTrend = async (maquinas, queryDate) => {};
 
 const getDates = cargo => {
     if (cargo == "analista") {

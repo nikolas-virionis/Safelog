125626022daa0b0dfe992eba4e73a1514eb7ef39
Lucas Mesquita de Souza <79462136+lucasmsouza24@users.noreply.github.com>
2021-12-02 15:16:26 -0300

    Update ac3.R

--- calc/ac3.R
+++ calc/ac3.R
@@ -1,5 +1,10 @@
-# correlação
+library(ggplot2)
 
+# correlação CPU percent - RAM LIVRE
+x <- amostraDesktop1$cpu_porcentagem
+y <- amostraDesktop1$cpu_frequencia
+dados <- data.frame(cpu_porcentagem=x, cpu_frequencia=y)
+ggplot(dados, aes(x=dados$cpu_porcentagem, y=dados$cpu_frequencia)) + geom_point() + geom_smooth(method=lm, se = FALSE)
 
 # comportamento - porcentagem de cpu
 dados <- data.frame(x=1:578, y=x)

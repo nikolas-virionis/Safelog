dce56459cb363ddb3b993eb9ff2b889058f644f0
Nikolas B Virionis <79850973+nikolas-virionis@users.noreply.github.com>
2021-12-04 14:03:13 -0300

    Update get_mac_addr.py

--- Apps/Python/util/get_mac_addr.py
+++ Apps/Python/util/get_mac_addr.py
@@ -1,8 +1,9 @@
 from getmac import get_mac_address as mac_addr
 
-print(mac_addr())
 try:
     import pyperclip
     pyperclip.copy(mac_addr())
 except Exception:
-    print("Opa, como q vai esse linux ae")
+    print()
+    
+print(mac_addr())

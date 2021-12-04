617af55e65c274fb94786c4fabe64a69fff6f40e
Nikolas B Virionis <79850973+nikolas-virionis@users.noreply.github.com>
2021-12-04 16:35:21 -0300

    Update get_mac_addr.py

--- Apps/Python/util/get_mac_addr.py
+++ Apps/Python/util/get_mac_addr.py
@@ -3,7 +3,7 @@ from getmac import get_mac_address as mac_addr
 try:
     import pyperclip
     pyperclip.copy(mac_addr())
+    print(mac_addr())
 except Exception:
-    print()
+    print(mac_addr())
     
-print(mac_addr())

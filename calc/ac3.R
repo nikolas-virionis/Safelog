1cdf0f025146509474a1eb5d6ba3eb2b9326796b
lucasmsouza24 <lucas.msouza@bandtec.com.br>
2021-12-06 18:38:04 -0300

    (ac3) invertendo x e y

--- calc/ac3.R
+++ calc/ac3.R
@@ -1,10 +1,10 @@
 library(ggplot2)
 
 # correlação CPU percent - RAM LIVRE
-x <- amostraDesktop1$cpu_porcentagem
-y <- amostraDesktop1$cpu_frequencia
-dados <- data.frame(cpu_porcentagem=x, cpu_frequencia=y)
-ggplot(dados, aes(x=dados$cpu_porcentagem, y=dados$cpu_frequencia)) + geom_point() + geom_smooth(method=lm, se = FALSE)
+y <- amostraDesktop1$cpu_porcentagem
+x <- amostraDesktop1$cpu_frequencia
+dados <- data.frame(cpu_frequencia=x, cpu_porcentagem=y)
+ggplot(dados, aes(x=dados$cpu_frequencia, y=dados$cpu_porcentagem)) + geom_point() + geom_smooth(method=lm, se = FALSE)
 
 # comportamento - porcentagem de cpu
 dados <- data.frame(x=1:length(x), y=x)

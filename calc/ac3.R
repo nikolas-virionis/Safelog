9219fb948f5a229797e96c8e1457c3ca0475dd10
lucasmsouza24 <lucas.msouza@bandtec.com.br>
2021-12-06 15:55:24 -0300

    ac3 r

--- calc/ac3.R
+++ calc/ac3.R
@@ -1,11 +1,34 @@
 library(ggplot2)
 
-# correlação CPU percent - RAM LIVRE
+# correlação (cpu_percent, cpu_frequencia)
+
+# defining x and y
 x <- amostraDesktop1$cpu_porcentagem
 y <- amostraDesktop1$cpu_frequencia
 dados <- data.frame(cpu_porcentagem=x, cpu_frequencia=y)
+
+# view alfa and beta
+model1 <- lm(data=dados, formula=dados$cpu_frequencia~dados$cpu_porcentagem)
+
+# linear regression
 ggplot(dados, aes(x=dados$cpu_porcentagem, y=dados$cpu_frequencia)) + geom_point() + geom_smooth(method=lm, se = FALSE)
 
-# comportamento - porcentagem de cpu
-dados <- data.frame(x=1:578, y=x)
-ggplot(dados, aes(x=dados$x, y=dados$y)) + geom_smooth(method=lm, se = FALSE)
+# correlation
+cor(x, y)
+
+
+# correlação cpu_porcentagem, ram_livre)
+
+# ram livre
+y <- amostraDesktop1$ram_livre
+
+dados <- data.frame(cpu_porcentagem=x, ram_livre=y)
+
+# view alfa and beta
+model1 <- lm(data=dados, formula=dados$ram_livre~dados$cpu_porcentagem)
+
+# linear regression
+ggplot(dados, aes(x=dados$cpu_porcentagem, y=dados$ram_livre)) + geom_point() + geom_smooth(method=lm, se = FALSE)
+
+# correlation
+cor(x, y)
